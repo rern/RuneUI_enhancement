a8c668af759b01cbfaf32e2cad767936bc59a6f8
rern <rernrern@gmail.com>
2017-02-13 23:12:58 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -95,14 +95,19 @@ rm -v $path'js/custom.js'
 rm -v $path'js/vendor/hammer.min.js'
 rm -v $path'js/vendor/pnotify3.custom.min.js'
 
+title "Restore backup file ..."
+file='/srv/http/app/templates/playback.php'
+mv -v $file'.bak' $file
+
 # restore modified files #######################################
 sed -i -e 's|<title>RuneAudio - RuneUIe<\/title>|<title>RuneAudio - RuneUI<\/title>|
 ' -e '\|pnotify.css|,\|<!-- enhancement -->| d
 ' -e '\|barleft|,\|lyricfade| d
 ' -e '\|runelogo.svg| d
+' -e 's|<i class="fa fa-gear">|MENU <i class="fa fa-bars dx">|
 ' -e '\|dropdownbg| d
 ' -e 's|<button id="menu-settings" class="btn-default dropdown-toggle"|<a id="menu-settings" class="dropdown-toggle"|
-' -e 's|href="#"><i class="fa fa-gear"></i></button>|href="#">MENU <i class="fa fa-bars dx"></i></a>|
+' -e 's|href="#"><i class="fa fa-gear"></i></button>|href="#"><i class="fa fa-gear"></i></a>|
 ' -e '\|href="/"><i class="fa fa-play"| s|^<?php /\*||; \|href="/"><i class="fa fa-play"| s|\*/?>$||
 ' -e '\|"fa fa-code"><\/i> Development<\/a>| d
 ' -e '\|logo.png| s|^<?php /\*||; \|logo.png| s|\*/?>$||
@@ -112,10 +117,6 @@ sed -i -e 's|<title>RuneAudio - RuneUIe<\/title>|<title>RuneAudio - RuneUI<\/tit
 ' /srv/http/app/templates/header.php
 
 sed -i -e '\|pnotify3.custom.min.js|,\|hammer.min.js| d' /srv/http/app/templates/footer.php
-
-sed -i -e '\|<?php /\* enh| d
-' -e '\|enh \*/?>| d
-' -e '\|<div class="tab-content" id="content">|,\|</div><!-- enh -->| d' /srv/http/app/templates/playback.php
 
 if ! grep -qs 'logout.php' /srv/http/app/templates/header.php; then
 	sed -i 's/|ico|svg/|ico/' /etc/nginx/nginx.conf
