be7ba5455fa509c0e61874e040ff31991fc91e9c
rern <rernrern@gmail.com>
2017-02-13 23:21:07 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -104,11 +104,10 @@ sed -i -e 's|<title>RuneAudio - RuneUIe<\/title>|<title>RuneAudio - RuneUI<\/tit
 ' -e '\|pnotify.css|,\|<!-- enhancement -->| d
 ' -e '\|barleft|,\|lyricfade| d
 ' -e '\|runelogo.svg| d
-' -e 's|<i class="fa fa-gear">|MENU <i class="fa fa-bars dx">|
 ' -e '\|dropdownbg| d
 ' -e 's|<button id="menu-settings" class="btn-default dropdown-toggle"|<a id="menu-settings" class="dropdown-toggle"|
-' -e 's|href="#"><i class="fa fa-gear"></i></button>|href="#"><i class="fa fa-gear"></i></a>|
-' -e '\|href="/"><i class="fa fa-play"| s|^<?php /\*||; \|href="/"><i class="fa fa-play"| s|\*/?>$||
+' -e 's|href="#"><i class="fa fa-gear"></i></button>|href="#"><i class="fa fa-bars dx"></i></a>|
+' -e '\|href="/"><i class="fa fa-play"| s|^<?php /\*||; \|href="/">MENU <i class="fa fa-play"| s|\*/?>$||
 ' -e '\|"fa fa-code"><\/i> Development<\/a>| d
 ' -e '\|logo.png| s|^<?php /\*||; \|logo.png| s|\*/?>$||
 ' -e $'s|"tab"\')?>><i class="fa fa-folder-open"></i></a>|"tab"\')?>><i class="fa fa-music"></i> Library</a>|
