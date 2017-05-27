f165f97c57c87b3b47dc900da78c0c3a2bfe0c19
rern <rernrern@gmail.com>
2017-05-27 10:09:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -67,8 +67,8 @@ fi
 # install RuneUI enhancement #######################################
 title2 "Install $runeenh ..."
 title "Get files ..."
-wget -q --show-progress -O srv.tar.xz "https://github.com/rern/RuneUI_enhancement/blob/master/_repo/srv.tar.xz?raw=1"
-wget -q --show-progress -O uninstall_enha.sh "https://github.com/rern/RuneUI_enhancement/blob/master/uninstall_enha.sh?raw=1"
+wget -q --show-progress https://github.com/rern/RuneUI_enhancement/blob/master/_repo/srv.tar.xz
+wget -q --show-progress https://github.com/rern/RuneUI_enhancement/blob/master/uninstall_enha.sh
 chmod +x uninstall_enha.sh
 
 # extract files #######################################
