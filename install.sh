0d69eaa3cfef9cfa39047017c2f234a1d7ff53b9
rern <rernrern@gmail.com>
2017-08-20 22:01:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -132,8 +132,9 @@ sed -i -e '/<div class="tab-content">/ i\
 /\*
 ' -e '/<!-- LIBRARY PANEL -->/ i\enh \*/?>
 ' $playback
-# fix sort webradio
+
 runeui=/srv/http/assets/js/runeui.js
+# fix sort webradio
 if ! grep -q 'append(elems)' $runeui; then
     sed -i '/highlighted entry/ a\
             var elems = $("#database-entries li").detach().sort(function (a, b) {\
@@ -145,6 +146,24 @@ if ! grep -q 'append(elems)' $runeui; then
     sed -i 's/var u=$("span","#db-currentpath")/var elems=$("#database-entries li").detach().sort(function(e,t){return $(e).text().toLowerCase().localeCompare($(t).text().toLowerCase())});$("#database-entries").append(elems);var u=$("span","#db-currentpath")/
     ' ${runeui/.js/.min.js}
 fi
+# library browsing
+sed -i -e $'/(\'#open-panel-sx\').hasClass/{n
+s|^|//|
+a\                renderLibraryHome()
+}
+' -e $'/Genres\/\' + path/{n;n;n
+s|^|//|
+a\
+var folderSlash = folderHref = "";\
+var folder = path.split("/");\
+for (i = 0; i < folder.length; i++) \{\
+folderSlash += (i == 0) ? "" : "/";\
+folderSlash += folder[i].replace("'", "\\\\'");  // escapes single quote in getDB param\
+folderHref += (i == 0) ? "" : " / ";\
+\
+breadcrumb.html(folderHref);
+}
+' $runeui
 
 # for nginx svg support
 nginx=/etc/nginx/nginx.conf
