ce40a40b28107a019f67fff837ef16e1b75ed946
rern <rernrern@gmail.com>
2017-08-24 11:15:03 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -15,7 +15,7 @@
 # remove uninstall_enha.sh
 
 # import heading function
-wget -qN --no-check-certificate https://raw.githubusercontent.com/rern/title_script/master/title.sh; . title.sh; rm title.sh
+wget -qN --no-check-certificate https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 runeenh=$( tcolor "RuneUI Enhancement" )
 
 # check installed #######################################
