fc9334813bcca1a9c3b825c2ca085d7be3d0b2ec
rern <rernrern@gmail.com>
2017-09-01 22:10:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ rm master.zip /tmp/install/{.*,*.md,install.sh} &> /dev/null
 chown -R root:root /tmp/install
 chown -R http:http /tmp/install/srv/http
 chmod -R 644 /tmp/install
-chmod 755 /tmp/install/root/* /tmp/install/usr/local/bin/uninstall*
+chmod 755 /tmp/install/usr/local/bin/uninstall*
 
 cp -r /tmp/install/* /
 rm -r /tmp/install
