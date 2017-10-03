0f218b14bb0a50e42387412eae7e5835d8b6ce83
rern <rernrern@gmail.com>
2017-10-03 11:08:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ chown -R root:root /tmp/install
 chown -R http:http /tmp/install/srv
 chmod -R 755 /tmp/install
 
-cp -rp /tmp/install/* /
-rm -r /tmp/install
+mv -vf /tmp/install/* /
+rm -rf /tmp/install
 echo
 
 # modify files #######################################
