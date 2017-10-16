021738a2e195b29622c76a8603ac6eca1489003c
rern <rernrern@gmail.com>
2017-10-16 18:35:28 +0700

    Add files via upload

--- srv/http/lyrics.php
+++ srv/http/lyrics.php
@@ -1,34 +1,34 @@
 <?php
-$artist = urlencode($_GET['artist']);
-$song = urlencode($_GET['song']);
+$artist = urlencode( $_GET[ 'artist' ] );
+$song = urlencode( $_GET[ 'song' ] );
 
 // 1st
 $url = "http://makeitpersonal.co/lyrics?artist=$artist&title=$song";
-$Lyrics = @file_get_contents($url);
-if ($Lyric) die($Lyric);
+$lyrics = @file_get_contents( $url );
+if ( $lyrics ) die( $lyrics );
 
 // 2nd
 $url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=$artist&song=$song";
 // set option > post
-$context = stream_context_create(array(
+$context = stream_context_create( array(
 	'http' => array(
 		'ignore_errors' => true
 	)
 ));
-$contents = file_get_contents($url, false, $context);
+$contents = file_get_contents( $url, false, $context );
 // check response code error
 $code = http_response_code();
-if ($code !== 200) die('(Server temporarily unavailable.)');
+if ( $code !== 200 ) die( '(Server temporarily unavailable.)' );
 // check response trouble
-if (strpos($contents, 'SearchLyricDirect: No valid words') !== false) die('(Song name contains all common "stop words".<br>Query was ignored by server.)');
+if ( strpos( $contents, 'SearchLyricDirect: No valid words' ) !== false ) die( '(Song name contains all common "stop words".<br>Query was ignored by server.)' );
 // convert $contents > xml > array for fetching
 $parser = xml_parser_create();
-xml_parse_into_struct($parser, $contents, $vals, $index);
-xml_parser_free($parser);
+xml_parse_into_struct( $parser, $contents, $vals, $index );
+xml_parser_free( $parser );
 
 //$LyricChecksum = $vals[3]['value'];
 //$LyricID = $vals[5]['value'];
 //$LyricCovertArtUrl = $vals[13]['value'];
-$Lyric = $vals[19]['value'];
+$lyrics = $vals[ 19 ][ 'value' ];
 
-echo $Lyrics;
+echo $lyrics;
