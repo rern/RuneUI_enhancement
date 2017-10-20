7bc90860573286d6fe5d8c2713451693847b15e0
rern <rernrern@gmail.com>
2017-10-20 08:48:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,21 +13,7 @@ installstart $@
 mkdir /srv/http/assets/fonts/backup
 cp /srv/http/assets/fonts/* /srv/http/assets/fonts/backup &> /dev/null
 
-echo -e "$bar Get files ..."
-wgetnc https://github.com/rern/RuneUI_enhancement/archive/$branch.zip
-
-echo -e "$bar Install new files ..."
-rm -rf /tmp/install
-mkdir -p /tmp/install
-bsdtar --exclude='.*' --exclude='*.md' -xvf $branch.zip --strip 1 -C /tmp/install
-rm $branch.zip /tmp/install/* &> /dev/null
-
-chown -R http:http /tmp/install/srv
-chmod -R 755 /tmp/install
-
-cp -rfp /tmp/install/* /
-rm -rf /tmp/install
-echo
+getinstallzip
 
 # modify files #######################################
 echo -e "$bar Modify files ..."
