9013e40cf4d844ade61fb34e24b040f16ceae1b6
rern <rernrern@gmail.com>
2017-11-03 22:53:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ sed -i 's/==UTF-8/=UTF-8/' $midori
 [[ $( redis-cli get buildversion ) == 'beta-20160313' ]] && redis-cli set release 0.3 &> /dev/null
 # set library home database
 redis-cli hmset display bar checked time checked playbuttons checked coverart checked sharebutton checked volume checked volumebuttons checked nas \
-	\checked usb checked webradio checked albums checked artists checked composer checked genre checked spotify checked dirble checked jamendo checked  &> /dev/null
+	\checked usb checked webradio checked albums checked artists checked composer checked genre checked spotify checked dirble checked jamendo checked &> /dev/null
 
 installfinish $@
 
