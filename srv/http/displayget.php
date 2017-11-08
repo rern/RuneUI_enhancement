a54e85715a491439f857cb02719a9804a4c8f2c4
rern <rernrern@gmail.com>
2017-11-08 21:37:22 +0700

    Add files via upload

---
+++ srv/http/displayget.php
@@ -0,0 +1,5 @@
+<?php
+$redis = new Redis(); 
+$redis->pconnect( '127.0.0.1' );
+
+echo json_encode( $redis->hGetAll( 'display' ) );
---
+++ srv/http/displaysave.php
@@ -0,0 +1,26 @@
+<?php
+if ( isset( $_POST[ 'playback' ] ) ) {
+	$display = array( 'bar' => $_POST[ 'bar' ] ? 'checked' : '' );
+	$display[ 'time'] = $_POST[ 'time' ] ? 'checked' : '';
+	$display[ 'coverart'] = $_POST[ 'coverart' ] ? 'checked' : '';
+	$display[ 'volume'] = $_POST[ 'volume' ] ? 'checked' : '';
+	$display[ 'buttons'] = $_POST[ 'buttons' ] ? 'checked' : '';
+} else {
+	$display = array( 'bar' => $_POST[ 'bar' ] ? 'checked' : '' );
+	$display[ 'nas'] = $_POST[ 'nas' ] ? 'checked' : '';
+	$display[ 'usb'] = $_POST[ 'usb' ] ? 'checked' : '';
+	$display[ 'webradios' ] = $_POST[ 'webradios' ] ? 'checked' : '';
+	$display[ 'albums' ] = $_POST[ 'albums' ] ? 'checked' : '';
+	$display[ 'artists' ] = $_POST[ 'artists' ] ? 'checked' : '';
+	$display[ 'composer' ] = $_POST[ 'composer' ] ? 'checked' : '';
+	$display[ 'genre' ] = $_POST[ 'genre' ] ? 'checked' : '';
+	$display[ 'spotify' ] = $_POST[ 'spotify' ] ? 'checked' : '';
+	$display[ 'dirble' ] = $_POST[ 'dirble' ] ? 'checked' : '';
+	$display[ 'jamendo' ] = $_POST[ 'jamendo' ] ? 'checked' : '';
+}
+$redis = new Redis();
+$redis->connect( '127.0.0.1' );
+
+$set = $redis->hmset( 'display', $display );
+
+echo $set;
