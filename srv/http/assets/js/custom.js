234ae1a24b4b35e6a7bcae597e2263568f758fa9
rern <rernrern@gmail.com>
2017-11-11 10:03:35 +0700

    Update custom.js

--- srv/http/assets/js/custom.js
+++ srv/http/assets/js/custom.js
@@ -535,7 +535,13 @@ function refreshState() {
 			var bitrate = Number( GUI.json.bitrate );
 			if ( ext == 'DSF' || ext == 'DFF' ) {
 				bitdepth = 1;
-				sampling = GUI.json.audio.split(':')[ 0 ] / 5512.5;
+				var audio = GUI.json.audio.split(':')[ 0 ];
+				if ( audio[ 0 ] == 'd' ) { // mpd0.20 - as dsd128:2 / dsd256:2 ...
+					channel = '';
+					sampling = Number( audio.replace( /[^\d]/g, '' ) );
+				} else {
+					sampling = GUI.json.audio.split(':')[ 0 ] / 5512.5;
+				}
 				bitrate = sampling * 44.1;
 				sampling = 'DSD'+ sampling;
 			} else if ( ext == 'FLAC' || ext == 'WAV' || ext == 'ALAC' ) {
