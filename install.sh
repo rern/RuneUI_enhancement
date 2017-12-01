dea6ff924c7b61574a05a1f202aa2784ddce60a7
rern <rernrern@gmail.com>
2017-12-01 17:15:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ sed -i -e 's/RuneAudio - RuneUI/RuneUIe/
 <div id="barleft"></div>\
 <div id="barright"></div>\
 ' -e $'/class="home"/ i\
-    <a href="http://www.runeaudio.com/forum/raspberry-pi-f7.html" alt="RuneAudio"><img class="logo" src="<?=$this->asset('/img/runelogo.svg')?>"></a>\
+    <a href="http://www.runeaudio.com/forum/raspberry-pi-f7.html" target="_blank" alt="RuneAudio"><img class="logo" src="<?=$this->asset('/img/runelogo.svg')?>"></a>\
 	<?php $path = $_SERVER["REQUEST_URI"] == "/" ? "/#playback" : "/";?>
 ' -e 's|/#playback|<?=$path;?>|
 ' -e '/dropdown-menu/ a\
