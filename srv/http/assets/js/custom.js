c0805a799d43a4dfc1fb77f8ac1e9a64a15ce238
rern <rernrern@gmail.com>
2017-12-10 19:46:49 +0700

    Add files via upload

--- srv/http/assets/js/custom.js
+++ srv/http/assets/js/custom.js
@@ -90,10 +90,6 @@ $( '#playlist-entries' ).click( function( e ) {
 $( '#playsource' ).click( function() {
 	$( '#overlay-playsource-open' ).click();
 } );
-// additional play/pause by click
-$( '#coverart' ).click( function() {
-	$( '#play' ).click();
-} );
 // lastfm search
 $( '#currentartist' ).click( function() {
 	var artist = $( this ).text();
@@ -181,8 +177,7 @@ Hammer = propagating( Hammer ); // propagating.js fix e.stopPropagation()
 var $hammercontent = new Hammer( document.body );
 $hammercontent.on( 'swiperight', function() {
 	panelr( 'right' );
-} );
-$hammercontent.on( 'swipeleft', function() {
+} ).on( 'swipeleft', function() {
 	panelr( 'left' );
 } );
 
@@ -190,25 +185,37 @@ var $hammerbarleft = new Hammer( document.getElementById( 'barleft' ) );
 $hammerbarleft.on( 'swipe', function( e ) {
 	$( '#barleft' ).click();
 //	e.stopPropagation();
-} );
-$hammerbarleft.get( 'swipe' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
+} ).get( 'swipe' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
 
 var $hammerbarright = new Hammer( document.getElementById( 'barright' ) );
 $hammerbarright.on( 'swipe', function( e ) {
 	$( '#menu-top, #menu-bottom' ).toggle();
 //	e.stopPropagation();
-} );
-$hammerbarright.get( 'swipe' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
+} ).get( 'swipe' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
 
 // skip if in menu settings
 if ( /\/.*\//.test( location.pathname ) === true ) return
 
 var $hammerinfo = new Hammer( document.getElementById( 'info' ) );
+var $hammercoverart = new Hammer( document.getElementById( 'coverart' ) );
 $hammerinfo.on( 'swiperight', function( e ) {
 	$( '#previous' ).click();
 	e.stopPropagation();
+} ).on( 'swipeleft', function( e ) {
+	$( '#next' ).click();
+	e.stopPropagation();
 } );
-$hammerinfo.on( 'swipeleft', function( e ) {
+// touch play control
+$hammercoverart.on( 'tap', function( e ) {
+	$( '#play' ).click();
+	e.stopPropagation();
+} ).on( 'press', function( e ) {
+	$( '#stop' ).click();
+	e.stopPropagation();
+} ).on( 'swiperight', function( e ) {
+	$( '#previous' ).click();
+	e.stopPropagation();
+} ).on( 'swipeleft', function( e ) {
 	$( '#next' ).click();
 	e.stopPropagation();
 } );
@@ -257,8 +264,7 @@ $hammervolume.on( 'press', function( e ) {
 var $hammerlibrary = new Hammer( document.getElementById( 'panel-sx' ) );
 $hammerlibrary.on( 'tap', function( e ) {
 	if ( $( '.home-block-remove' ).length && !$( e.target ).is( 'span.block-remove' ) ) $( '#db-homeSetup' ).click();
-} );
-$hammerlibrary.on( 'press', function() {
+} ).on( 'press', function() {
 	if ( !$( '#db-currentpath' ).hasClass( 'hide' ) ) return
 	info( {
 		  title  : 'Libary Home'
@@ -640,91 +646,6 @@ function updateGUI() {
 		$('#cover-art').css('background-image','url("assets/img/cover-radio.jpg")');
 	}
 		
-// 0.4b only
-	if ( !$( '#songinfo-modal' ).length ) return;
-// -------------------------------------------------------------------------------
-//	$( '#lyric-text-overlay' ).html( lyrics );
-    var localbrowser = $( '#playback-ss' ).length;
-	if ( localbrowser ) {
-		if (GUI.stream !== 'radio') {
-			$('#currentartist-ss').html(!currentartist ? '<span class="notag">[no artist]</span>' : currentartist);
-			if (!currentsong || currentsong.length > 35) {
-				$('#currentsong-ss')[0].style.fontSize = "26px";
-			} else if (currentsong.length > 25) {
-				$('#currentsong-ss')[0].style.fontSize = "33px";
-			} else {
-				$('#currentsong-ss')[0].style.fontSize = "40px";
-			}
-			$('#currentsong-ss').html(!currentsong ? '<span class="notag">[no title]</span>' : currentsong);
-			if (currentalbum === null || currentalbum.length > 45) {
-				$('#currentalbum-ss')[0].style.fontSize = "20px";
-			} else if (currentalbum.length > 30) {
-				$('#currentalbum-ss')[0].style.fontSize = "24px";
-			} else {
-				$('#currentalbum-ss')[0].style.fontSize = "30px";
-			}
-			$('#currentalbum-ss').html(!currentalbum ? '<span class="notag">[no album]</span>' : currentalbum);
-		} else {
-			$('#currentartist-ss').html(!currentartist ? radioname : currentartist);
-			$('#currentsong-ss').html(!currentsong ? radioname : currentsong);
-			$('#currentalbum-ss').html('<span class="notag">streaming</span>');
-		}
-	}
-			
-	if (GUI.old_state === GUI.state && GUI.currentalbum === currentalbumstring) return;
-// ------------------------------------------------------------------------------
-	GUI.old_state = GUI.state;
-	$('#artist-bio-ss, #addinfo-text-ss, #artist-bio-overlay, #addinfo-text-overlay').html('');
-	$('#artist-image-ss, #artist-image-overlay').css('background-image', '');
-	if (GUI.stream !== 'radio') {
-		if ( localbrowser ) $('#cover-art-ss').css('background-image','url("/coverart/?v=' + covercachenum + '")');
-		$.ajax({
-			url: '/artist_info/',
-			success: function(data){
-				var info = jQuery.parseJSON(data);
-				if (typeof info.artist !== 'undefined' && info.artist.bio.content !== '') {
-					$('#artist-bio-overlay').html(info.artist.bio.summary);
-					$('#artist-bio-full-overlay').html(info.artist.bio.content);
-					$('#artist-image-overlay').css('background-image', 'url("' + info.artist.image[2]["#text"] + '")');
-					$('#addinfo-text-overlay').html('Similar Artists:<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[0].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[1].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[2].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[3].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[4].name + '<br>&nbsp;<br>&nbsp;');
-
-					if ( !localbrowser ) return;
-					// --------------------------------------
-					$('#artist-bio-ss').html(info.artist.bio.content.substring(0,550) + ' ... ');
-					$('#addinfo-text-ss').html('Similar Artists:<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[0].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[1].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[2].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[3].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[4].name);
-					$('#artist-image-ss').css('background-image', 'url("' + info.artist.image[2]["#text"] + '")');
-				} else {
-					$('#artist-bio-overlay').html(' sorry, no info available ');
-					$('#addinfo-text-overlay').html('');
-					$('#artist-image-overlay').css('background-image','url("assets/img/unkown.png")');
-					
-					if ( !localbrowser ) return;
-					// --------------------------------------
-					$('#artist-bio-ss').html(' sorry, no info available ');
-					$('#addinfo-text-ss').html('');
-					$('#artist-image-ss').css('background-image','url("assets/img/unkown.png")');
-				}
-			},
-			cache: false
-		});
-	} else {
-		if ( !localbrowser ) return;
-		// --------------------------------------
-		$('#cover-art-ss').css('background-image','url("assets/img/cover-radio.jpg")');
-		$.ajax({
-			url: '/artist_info/',
-			success: function(data){
-				var info = jQuery.parseJSON(data);
-				if (typeof info.artist !== 'undefined' && info.artist.bio.content !== '') {
-					$('#artist-bio-ss').html(info.artist.bio.content.substring(0,1000) + ' ... ');
-					$('#addinfo-text-ss').html('Similar Artists:<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[0].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[1].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[2].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[3].name + '<br>&nbsp;&nbsp;&nbsp;&nbsp;' + info.artist.similar.artist[4].name);
-					$('#artist-image-ss').css('background-image', 'url("' + info.artist.image[2]["#text"] + '")');
-				}
-			},
-			cache: false
-		});
-	}
-// 0.4b only
 }
 
 function compareAB( a, b, prop ) {
