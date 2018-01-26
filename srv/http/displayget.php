aa1b65c07f9518dbc4047c3faad4357f70db7879
rern <rernrern@gmail.com>
2018-01-26 20:21:42 +0700

    Update displayget.php

--- srv/http/displayget.php
+++ srv/http/displayget.php
@@ -2,4 +2,6 @@
 $redis = new Redis(); 
 $redis->pconnect( '127.0.0.1' );
 
+$display = $redis->hGetAll( 'display' );
+if ( $redis->get( 'volume' ) == 0 ) $display[ 'volume' ] = '';
 echo json_encode( $redis->hGetAll( 'display' ) );
