c1edcebf4df1a0097b85bd88f896b37cbb008769
rern <rernrern@gmail.com>
2018-01-27 09:34:31 +0700

    Add files via upload

--- srv/http/displayget.php
+++ srv/http/displayget.php
@@ -3,5 +3,5 @@ $redis = new Redis();
 $redis->pconnect( '127.0.0.1' );
 
 $display = $redis->hGetAll( 'display' );
-if ( $redis->get( 'volume' ) == 0 ) $display[ 'volume' ] = '';
+$display[ 'volumempd' ] = $redis->get( 'volume' );
 echo json_encode( $display );
