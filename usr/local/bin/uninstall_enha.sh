18ee8a7a125084ce721af4c8e7d8694a5f85fd01
rern <rernrern@gmail.com>
2018-01-28 09:41:26 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -39,10 +39,10 @@ sed -i -e 's/RuneUIe/RuneAudio - RuneUI/
 ' -e '/dropdownbg/ d
 ' -e 's/<button id="menu-settings" class="btn-default dropdown-toggle"/<a id="menu-settings" class="dropdown-toggle"/
 ' -e 's|href="#"><i class="fa fa-gear"></i></button>|href="#">MENU <i class="fa fa-bars dx"></i></a>|
-' -e '\|href="/"><i class="fa fa-play"| s|^<?php /\*||; \|href="/"><i class="fa fa-play"| s|\*/?>$||
+' -e '\|href="/"><i class="fa fa-play"| s|^<?php /\*||; s|\*/?>$||
 ' -e 's|id="open-panel-dx"><a|& href="/#panel-dx"|
 ' -e '\|href="http://www.runeaudio.com|, \|</a>| d
-' -e '/logo.png/ s|^<?php /\*||; /logo.png/ s|\*/?>$||
+' -e '/logo.png/ s|^<?php /\*||; s|\*/?>$||
 ' -e '\|"fa fa-code"></i> Development</a>| d
 ' -e 's|id="open-panel-sx"><a|& href="/#panel-sx"|
 ' -e 's|id="open-playback".*><a|& href="/#playback"|
