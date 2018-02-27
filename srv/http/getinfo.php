4572d668e9f4918ae1f05dae1c011b492f7e3398
rern <rernrern@gmail.com>
2018-02-27 17:54:19 +0700

    Update getinfo.php

--- srv/http/getinfo.php
+++ srv/http/getinfo.php
@@ -1,8 +1,8 @@
 <?php
-$artist = $_POST[ 'artist' ];;
+$artist = $_POST[ 'artist' ];
 
 $ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=$artist&api_key=ba8ad00468a50732a3860832eaed0882&format=json" ); // set url
+curl_setopt( $ch, CURLOPT_URL, "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=$artist&api_key=ba8ad00468a50732a3860832eaed0882&format=json" );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 ); //return the transfer as a string 
 $data = curl_exec( $ch );
 curl_close( $ch );
