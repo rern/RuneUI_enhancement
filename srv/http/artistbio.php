d82521282cd32e0ac4a77b03964ed2768262bf35
rern <rernrern@gmail.com>
2018-03-01 14:37:29 +0700

    Add files via upload

--- srv/http/artistbio.php
+++ srv/http/artistbio.php
@@ -1,8 +1,10 @@
 <?php
+$apikey = 'ba8ad00468a50732a3860832eaed0882';
 $artist = $_GET[ 'artist' ];
+$method = '&method=artist.getinfo&artist='.$artist;
 
 $ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=$artist&api_key=ba8ad00468a50732a3860832eaed0882&format=json" );
+curl_setopt( $ch, CURLOPT_URL, 'http://ws.audioscrobbler.com/2.0/?format=json&api_key='.$apikey.$method );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 ); //return the transfer as a string 
 $data = curl_exec( $ch );
 curl_close( $ch );
@@ -11,10 +13,13 @@ $data = preg_replace( '/\\\\n/', '<br>', $data );
 $data = preg_replace( '/\\\\"/', '&quot;', $data );
 
 $data = json_decode( $data, True );
-$image = $data[ 'artist' ][ 'image' ][ 5 ][ '#text' ];
-$content = preg_replace( '/ <a.*Read more on Last.fm.*/', '', $data[ 'artist' ][ 'bio' ][ 'content' ] );
-$similar =  $data[ 'artist' ][ 'similar' ][ 'artist' ];
-$similars = '<sapn>Similar Artists:</span><br>';
+print_r($data['album']);
+$data = $data[ 'artist' ];
+$image = $data[ 'image' ][ 5 ][ '#text' ];
+$genre = ucwords( $data[ 'tags' ][ 'tag' ][ 0 ][ 'name' ] );
+$content = preg_replace( '/ <a.*Read more on Last.fm.*/', '', $data[ 'bio' ][ 'content' ] );
+$similar =  $data[ 'similar' ][ 'artist' ];
+$similars = '';
 foreach ( $similar as $name ) {
 	$similars.= '
 		<div id="biosimilar">
@@ -31,6 +36,9 @@ echo '
 		<p>
 			'.$content.'
 			<br>
+			<span>Genre: </span>'.$genre.'<br>
+			<br>
+			<span>Similar Artists:</span><br>
 			'.$similars.'
 		</p>
 	</form>
