77a7ac2042f06a748a6562ccbf829dfc7d5f6191
rern <rernrern@gmail.com>
2018-03-18 21:43:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ enha*/
 
 file=/srv/http/assets/js/runeui.min.js
 echo $file
-sed -i -e 's|e("#time").knob|/\*enha &|
+sed -i -e 's|,e("#time").knob\)|;/\*enha &|
 ' -e 's|var r=e("#playlist-entries")| enha*/&|
 ' $file
 
