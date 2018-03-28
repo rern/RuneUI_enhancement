541762c90f50549cc904cec308c843861948c47f
rern <rernrern@gmail.com>
2018-03-28 12:23:18 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -40,7 +40,8 @@ sed -i -e 's|RuneUIe|RuneAudio - RuneUI|
 ' -e '/roundslider.min.css\|enhance.css\|id="bartop"\|id="barbottom\|dropdownbg\|button id="menu-settings"\|id="pause"\|Development<.a>/ d
 ' -e '/a href="http:..www.runeaudio.com/, /<.a>/ d
 ' -e 's/data-cmd="toggle"/data-cmd="play"/
-' -e 's|^<?php /\*||; s|\*/?>$||
+' -e 's|<!--enha||; s|enha-->||
+' -e '/<!--0enha/,/1enha-->/ d
 ' -e 's|id="open-panel-sx"><a |&href="/#panel-sx"|
 ' -e 's|id="open-playback".*><a |&href="/#playback"|
 ' -e 's|id="open-panel-dx"><a |&href="/#panel-dx"|
