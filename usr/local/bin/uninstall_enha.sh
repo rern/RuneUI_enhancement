3bcadcb1ce30e5895d473fb4d844209f2b685296
rern <rernrern@gmail.com>
2018-04-05 16:51:43 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -40,7 +40,7 @@ sed -i -e 's|RuneUIe|RuneAudio - RuneUI|
 ' -e '/roundslider.min.css\|enhance.css\|id="bartop"\|id="barbottom\|dropdownbg\|button id="menu-settings"\|id="pause"\|Development<.a>/ d
 ' -e '/a href="http:..www.runeaudio.com/, /<.a>/ d
 ' -e 's/data-cmd="toggle"/data-cmd="play"/
-' -e 's|<!--enha||; s|enha-->||
+' -e 's/<!--enha\|enha-->//g
 ' -e '/<!--0enha-->/,/<!--1enha-->/ d
 ' -e 's|id="open-panel-sx"><a |&href="/#panel-sx"|
 ' -e 's|id="open-playback".*><a |&href="/#playback"|
@@ -52,7 +52,7 @@ sed -i -e 's|RuneUIe|RuneAudio - RuneUI|
 
 file=/srv/http/app/templates/footer.php
 echo $file
-sed -i -e 's|<!--enha||; s|enha-->||' $file
+sed -i -e 's/<!--enha\|enha-->//g
 ' -e '/vendor\/jquery-ui.min.js/ d
 ' $file
 
