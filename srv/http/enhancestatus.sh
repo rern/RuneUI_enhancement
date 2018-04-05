e794b9e41c48dcbe872507b8332792a0d57928f7
rern <rernrern@gmail.com>
2018-04-05 17:47:58 +0700

    Add files via upload

--- srv/http/enhancestatus.sh
+++ srv/http/enhancestatus.sh
@@ -1,76 +1,59 @@
 #!/bin/bash
 
-toSecond() {
-	echo $1 | awk -F':' '{ if ( NF == 2 ) { print $1 * 60 + $2 } else { print $1 * 60 * 60 + $2 * 60 + $3 } }'
-}
+state=$( mpc status | grep ')' | sed 's/\[\(.*\)\].*/\1/' )
+[[ ! $state ]] && state=stop
 
-if mpc status | grep ')'; then # playing or paused - get all with 'status' both mpd protocol and mpc
-	data=( $( { echo status; sleep 0.1; } | telnet localhost 6600 | grep "bitrate\|audio" | tr -d '\n' | sed 's/bitrate: *//; s/audio: *\|:/ /g' ) )
-	data+=( $( mpc status | grep ')' | sed 's/\] *#.*   *\|\// /g; s/\[\| (.*)//g' ) )
-	
-	bitdepth=${data[2]}
-	[[ $bitdepth == 'dsd' ]] && bitdepth=1
-	samplerate=$( perl -E "say ${data[1]} / 1000" )
-	bitrate=$( perl -E "say ${data[2]} * $samplerate * ${data[3]}" )
-	bitrate=$( printf "%.0f" $bitrate )
-	sampling="$bitdepth bit $samplerate kHz $bitrate kbit/s"
-	elapsed=$( toSecond ${data[5]} )
-	time=$( toSecond ${data[6]} )
-	state=${data[4]}
+if [[ $state != stop ]]; then
+	elapsed=( $( mpc status | grep ')' | sed 's/.*#.*   *//; s|/.*||' ) )
+	elapsed=$( echo $elapsed | awk -F':' '{ if ( NF == 2 ) { print $1 * 60 + $2 } else { print $1 * 60 * 60 + $2 * 60 + $3 } }' )
 else
-	# stop - get file and time with 'currentsong' mpd protocol
-	filetime=$( { echo currentsong; sleep 0.1; } | telnet localhost 6600 | grep 'file:\|Time:' | sed 's|file: |/mnt/MPD/|' )
-	file=${filetime/Time:*}
-	ext=$( echo $file | sed 's/^.*\.//' | tr '[:lower:]' '[:upper:]' )
 	elapsed=0
-	time=${filetime#*Time: }
-	state='stop'
+fi
+
+filetime=$( { echo currentsong; sleep 0.1; } | telnet localhost 6600 | grep 'file:\|Time:' | sed 's|file: |/mnt/MPD/|' )
+file=${filetime/Time:*}
+ext=$( echo $file | sed 's/^.*\.//' | tr '[:lower:]' '[:upper:]' )
+time=${filetime#*Time: }
+
+# not DSD - get sampling with 'soxi'
+if [[ $ext != DSF && $ext != DFF ]]; then
+	IFS0=$IFS
+	IFS=$( echo -en "\n\b" )
+	data=$( soxi $file | grep 'Channels\|Sample Rate\|Precision' | tr -d '\n' )
+	IFS=$IFS0
 	
-	if [[ $ext == DFF ]]; then
-		echo '{ "sampling": "'&nbsp;'", "elapsed": "0", "time": "0", "state": "stop" }'
-		exit
+	audio=( $( echo $data | sed 's/Channels : \|Sample Rate :\|Precision :\|-bit//g' ) )
+	channel=${audio[0]}
+	bitdepth=${audio[2]}
+	samplerate=$( python -c "print( ${audio[1]} / 1000 )" )
+	bitrate=$( python -c "print( $channel * $bitdepth * $samplerate )" )
+	sampling="$bitdepth bit $samplerate kHz $bitrate kbit/s"
+	if (( ${bitrate%.*} >= 1000 )); then
+		bitrate=$( python -c "print( round( $bitrate / 1000, 2 ) )" )
+		sampling="$bitdepth bit $samplerate kHz $bitrate Mbit/s"
 	fi
-	
-	# not DSD - get sampling with 'soxi'
-	if [[ $ext != DSF && $ext != DFF ]]; then
-		IFS0=$IFS
-		IFS=$( echo -en "\n\b" )
-		data=$( soxi $file | grep 'Channels\|Sample Rate\|Precision' | tr -d '\n' )
+# DSD - get sampling by 'hexdump'
+else
+	# dsd64  =  2822400 <-> 002b1100
+	# dsd128 =  5644800 <-> 00562200
+	# dsd256 = 11289600 <-> 00AC4400
+	# dsd512 = 22579200 <-> 01588800
+	IFS0=$IFS
+	IFS=$( echo -en "\n\b" )
+	if [[ $ext == DSF ]]; then
+		hexword=$( hexdump -x -s56 -n4 $file )         # strin=0000040 <5758> <5960> 000003c
 		IFS=$IFS0
-		
-		audio=( $( echo $data | sed 's/Channels : \|Sample Rate :\|Precision :\|-bit//g' ) )
-		channel=${audio[0]}
-		bitdepth=${audio[2]}
-		samplerate=$( python -c "print( ${audio[1]} / 1000 )" )
-		bitrate=$( python -c "print( $channel * $bitdepth * $samplerate )" )
-		sampling="$bitdepth bit $samplerate kHz $bitrate kbit/s"
-		if (( ${bitrate%.*} >= 1000 )); then
-			bitrate=$( python -c "print( round( $bitrate / 1000, 2 ) )" )
-			sampling="$bitdepth bit $samplerate kHz $bitrate Mbit/s"
-		fi
-	# DSD - get sampling by 'hexdump'
-	else
-		# dsd64  =  2822400 <-> 002b1100
-		# dsd128 =  5644800 <-> 00562200
-		# dsd256 = 11289600 <-> 00AC4400
-		# dsd512 = 22579200 <-> 01588800
-		IFS0=$IFS
-		IFS=$( echo -en "\n\b" )
-		if [[ $ext == DSF ]]; then
-			hexword=$( hexdump -x -s56 -n4 $file ) # strin=0000040 <5758> <5960> 000003c
-			IFS=$IFS0
-			hex=( $( echo $hexword | cut -d' ' -f2,3 ) ) # array=( <5758> <5960> )
-			bitrate=$( echo $(( 16#${hex[1]}${hex[0]} )) ) # bitrate byte order: #59#60#57#58
-		else # DFF
-			hexword=$( hexdump -x -s60 -n4 $file )  # string=000003c <6162> <6364> 0000040
-			IFS=$IFS0
-			hex=( $( echo $hexword | cut -d' ' -f2,3 | tr -d ' ' | sed 's/.\{2\}/& /g' ) ) # array=( <61> <62> <63> <64> )
-			bitrate=$( echo $(( 16#${hex[1]}${hex[0]}${hex[3]}${hex[2]} )) ) # bitrate byte order: #62#61#64#63
-		fi
-		dsd=$(( bitrate / 44100 ))
-		Mbps=$( python -c "print( round( $bitrate / 1000000, 2 ) )" )
-		sampling="1 bit DSD$dsd - $Mbps Mbit/s"
+		hex=( $( echo $hexword | cut -d' ' -f2,3 ) )   # array=( <5758> <5960> )
+		bitrate=$( echo $(( 16#${hex[1]}${hex[0]} )) ) # bitrate byte order: #59#60#57#58
+	else # DFF
+		hexword=$( hexdump -x -s60 -n4 $file )                                         # string=000003c <6162> <6364> 0000040
+		IFS=$IFS0
+		hex=( $( echo $hexword | cut -d' ' -f2,3 | tr -d ' ' | sed 's/.\{2\}/& /g' ) ) # array=( <61> <62> <63> <64> )
+		bitrate=$( echo $(( 16#${hex[1]}${hex[0]}${hex[3]}${hex[2]} )) )               # bitrate byte order: #62#61#64#63
 	fi
+	dsd=$(( bitrate / 44100 ))
+	Mbps=$( python -c "print( round( $bitrate / 1000000, 2 ) )" )
+	sampling="1 bit DSD$dsd - $Mbps Mbit/s"
 fi
 
 echo '{ "sampling": "'$sampling'", "elapsed": "'$elapsed'", "time": "'$time'", "state": "'$state'" }'
