4cef47224afe9cd6c9fec7465d7a2a3fa2650bbe
rern <rernrern@gmail.com>
2018-05-22 08:09:17 +0700

    Add files via upload

--- srv/http/enhanceradio.sh
+++ srv/http/enhanceradio.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mpc play
-
-$i=0
-until [[ sampling=$( { echo status; sleep 0.1; } | telnet localhost 6600 | grep 'bitrate\|audio' ) || $i == 10 ]]; do
+sampling=
+i=0
+until [[ i -eq 10 ]]; do
+	sampling=$( { echo status; sleep 0.1; } | telnet localhost 6600 | grep 'bitrate\|audio' )
 	(( i++ ))
+	[[ -n $sampling && $sampling != 'bitrate: 0' ]] && break || sleep 0.5
 done
 
 mpc stop
@@ -12,6 +14,6 @@ mpc stop
 bitrate=$( echo $sampling | cut -d' ' -f2 )
 bitdepth=$( echo $sampling | cut -d' ' -f4 | cut -d':' -f2 )
 Hz=$( echo $sampling | cut -d' ' -f4 | cut -d':' -f1 )
-kHz=$( awk "BEGIN { printf \"%.1f\n\", $Hz / 1000 }" )
+kHz=$( awk "BEGIN { printf \"%.1f\n\", "$Hz" / 1000 }" )
 
 echo $bitdepth bit $kHz kHz $bitrate kbit/s
