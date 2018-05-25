b612809efc2921a1bd148cd6fb23fb314dd136cc
rern <rernrern@gmail.com>
2018-05-25 12:08:44 +0700

    Update enhance.js

--- srv/http/assets/js/enhance.js
+++ srv/http/assets/js/enhance.js
@@ -1474,7 +1474,7 @@ function setinfo() {
 		} else {
 			$( '#currentartist' ).html( GUI.json.radioname );
 			$( '#currentsong' ).html( GUI.state !== 'stop' ? GUI.json.currentsong : '&nbsp;' );
-			$( '#currentalbum' ).html( 'Streaming' );
+			$( '#currentalbum' ).html( 'Webradio' );
 		}
 		
 		if ( GUI.json.song ) {
