7bbee8ac5698566eb0463a6e41fc248b54d830e3
rern <rernrern@gmail.com>
2018-05-28 13:48:09 +0700

    Delete enhanceradioname.sh

--- srv/http/enhanceradioname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-url=${1//\//.}
-radio=$( redis-cli hgetall webradios )
-valueline=$( echo "$radio" | sed -n "/$url/ =" )
-keyline=$(( valueline - 1 ))
-name=$( echo "$radio" | sed "$keyline q;d" )
-
-echo $name
