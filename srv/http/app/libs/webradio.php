2c06775c6dfc5ab044bee8bdef19f42505f77123
rern <rernrern@gmail.com>
2018-06-03 13:25:04 +0700

    Update webradio.php

--- srv/http/app/libs/webradio.php
+++ srv/http/app/libs/webradio.php
@@ -1,22 +1,34 @@
 <?php
+function getInfo( $mpd ) {
+sendMpdCommand( $mpd, 'status' );
+$status = readMpdResponse( $mpd );
+sendMpdCommand( $mpd, 'currentsong' );
+$status.= readMpdResponse( $mpd );
+$line = strtok( $status, PHP_EOL );
+while ( $line !== false ) {
+	$pair = explode( ' ', $line );
+	$key = substr( $pair[ 0 ], 0, -1 );       // remove ':'
+	$val = $pair[ 1 ];
+	if ( $key !== 'O' ) $info[ $key ] = $val; // remove 'OK' lines
+	if ( $key === 'audio') {
+		$audio = explode( ':', $val );
+		$info[ 'bitdepth' ] = $audio[ 1 ];
+		$info[ 'sampling' ] = $audio[ 0 ];
+	}
+	$line = strtok( PHP_EOL );
+}
+if ( array_key_exists( 'bitrate', $info ) ) {
+	$samplinginfo = substr( $info[ 'file' ], 0, 4 ) === 'http' ? '' : $info[ 'bitdepth' ].' bit ';
+	$samplinginfo.= round( $info[ 'sampling' ] / 1000, 1 ).' kHz '.$info[ 'bitrate' ].' kbit/s';
+	$info[ 'samplinginfo' ] = $samplinginfo;
+} else {
+	$info[ 'samplinginfo' ] = '';
+}
+	return $info;
+}
 function addRadio( $mpd, $redis, $data ) {
 	if ( $data->label === '' || $data->url === '' ) return;
     
-	ui_notify( 'Webradio', 'Testing connection ...' );
-	$data = shell_exec( "{ echo play; sleep 3; echo status; echo stop; sleep 0.1; } | telnet localhost 6600 | grep 'bitrate\|audio' | sed 's/^.*: //; s/:.*:.*$//'" );
-	$data = explode( "\n", $data );
-	if ( !$data[ 0 ] ) {
-		ui_notify( 'Webradio', "URL Connection FAILED!." );
-		return;
-	}
-	
-	$sampling = round( $data[ 1 ] / 1000 , 1 ).' kHz '.$data[ 0 ].' kbit/s';
-	ui_notify( 'Webradio', $sampling );
-	
-	$redis->hSet( 'webradiosampling', $data->label, $sampling );
-	$redis->hSet( 'webradioname', $data->url, $data->label );
-	$redis->hSet( 'webradios', $data->label, $data->url );
-	
 	$file = '/mnt/MPD/Webradio/'.$data->label.'.pls';
 	$newpls = "[playlist]\n"
 		."NumberOfEntries=1\n"
@@ -25,6 +37,26 @@ function addRadio( $mpd, $redis, $data ) {
 	$fp = fopen( $file, 'w' );
 	fwrite( $fp, $newpls );
 	fclose( $fp );
+
+	ui_notify( 'Webradio', 'Testing connection ...' );
+	addToQueue( $mpd, 'Webradio/'.$data->label.'.pls' );
+	sleep( 1 );
+	$last = getInfo()[ 'playlistlength' ] - 1;
+	sendMpdCommand( $mpd, 'play '.$last );
+	sleep( 3 );
+	$samplinginfo = getInfo()[ 'samplinginfo' ];
+	if ( !$samplinginfo ) {
+		ui_notify( 'Webradio', "URL Connection FAILED!." );
+		unlink( $file );
+		return;
+	}
+	
+	ui_notify( 'Webradio', $samplinginfo );
+	
+	$redis->hSet( 'webradiosampling', $data->label, $samplinginfo );
+	$redis->hSet( 'webradioname', $data->url, $data->label );
+	$redis->hSet( 'webradios', $data->label, $data->url );
+	
 	
 	sendMpdCommand( $mpd, 'update Webradio' );
 }
@@ -34,14 +66,13 @@ function editRadio( $mpd, $redis, $data ) {
 	
 	if ( !unlink( '/mnt/MPD/Webradio/'.$data->label.'.pls' ) ) return;
 	
-	$urldel = $redis->hGet('webradios', $data->label);
-	$redis->hDel('webradioname', $urldel);
-	$redis->hDel('webradiosampling', $data->label);
-	$redis->hDel('webradios', $data->label);
-	
+	$urldel = $redis->hGet('webradios', $data->label );
+	$redis->hDel( 'webradioname', $urldel );
+	$redis->hDel( 'webradiosampling', $data->label );
+	$redis->hDel( 'webradios', $data->label );
 	$data->label = $data->newlabel;
 	$data->newlabel = null;
-	addRadio($mpd, $redis, $data);
+	addRadio( $mpd, $redis, $data );
 	
 	sendMpdCommand($mpd, 'update Webradio');
 }
