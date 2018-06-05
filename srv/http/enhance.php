e4b44677b2a9f68e40d77e372ace3f9b62dedcef
rern <rernrern@gmail.com>
2018-06-05 11:21:41 +0700

    Add files via upload

--- srv/http/enhance.php
+++ srv/http/enhance.php
@@ -1,11 +1,10 @@
 <?php
-// redis: command = { dataid: [ 'command', 'key', ['hash',] 'value' ] }
 // bash: command = { dataid: [ '/fullpath/command argument' ] }
+// redis: command = { dataid: [ 'command', 'key', ['hash',] 'value' ] }
 // { volume: N } ( mute/unmute: N = -1 )
-//
-// $.post( '/enhanceredis.php', { JSON.stringify( command ) }, ...
+// [no parameters] for status
 
-// single bash
+// bash
 if ( isset( $_POST[ 'bash' ] ) ) {
 	$result = exec( '/usr/bin/sudo '.$_POST[ 'bash' ] );
 	echo $result;
@@ -15,50 +14,60 @@ if ( isset( $_POST[ 'bash' ] ) ) {
 $redis = new Redis(); 
 $redis->pconnect( '127.0.0.1' );
 
-// volume
-if ( isset( $_POST[ 'volume' ] ) ) {
-	include( '/srv/http/app/libs/runeaudio.php' );
-	$mpd = openMpdSocket('/run/mpd.sock');
-// normal
-	if ( $_POST[ 'volume' ] != -1 ) {
-		sendMpdCommand( $mpd, 'setvol '.$_POST[ 'volume' ] );
-		$redis->set( 'volumemute', 0 );
-	} else {
-// mute / unmute
-		$volumemute = $redis->get( 'volumemute' );
-		if ( $volumemute == 0 ) {
-			$cmdlist = "command_list_begin\n"
-				."status\n"
-				."setvol 0\n"
-				."command_list_end";
-			sendMpdCommand( $mpd, $cmdlist );
-			$status = readMpdResponse( $mpd );
-			$vol = arrayLines( $status )[ 'volume' ];
-			$redis->set( 'volumemute', $vol );
-			echo $vol;
-		} else {
-			sendMpdCommand( $mpd, 'setvol '.$volumemute );
-			$redis->set( 'volumemute', 0 );
-			echo $volumemute;
+// redis
+if ( isset( $_POST[ 'redis' ] ) ) {
+	$array = json_decode( $_POST[ 'redis' ], true );
+	foreach ( $array as $field => $arg ) {
+		$count = count( $arg );
+		$command = $arg[ 0 ];
+		$key = $arg[ 1 ];
+		
+		if ( $count === 2 ) {
+			$result[ $field ] = $redis->$command( $key );
+		} else if ( $count === 3 ) {
+			$result[ $field ] = $redis->$command( $key, $arg[ 2 ] );
+		} else if ( $count === 4 ) {
+			$result[ $field ] = $redis->$command( $key, $arg[ 2 ], $arg[ 3 ] );
 		}
 	}
+	echo json_encode( $result );
 	die();
 }
 
 
-$array = json_decode( $_POST[ 'redis' ], true );
+include( '/srv/http/app/libs/runeaudio.php' );
+$mpd = openMpdSocket('/run/mpd.sock');
 
-foreach ( $array as $field => $arg ) {
-	$count = count( $arg );
-	$command = $arg[ 0 ];
-	$key = $arg[ 1 ];
-	
-	if ( $count === 2 ) {
-		$result[ $field ] = $redis->$command( $key );
-	} else if ( $count === 3 ) {
-		$result[ $field ] = $redis->$command( $key, $arg[ 2 ] );
-	} else if ( $count === 4 ) {
-		$result[ $field ] = $redis->$command( $key, $arg[ 2 ], $arg[ 3 ] );
-	}
+// MPD telnet command (protocol)
+if ( isset( $_POST[ 'mpd' ] ) ) {
+	sendMpdCommand( $mpd, $_POST[ 'mpd' ] );
+	die();
 }
-echo json_encode( $result );
+
+// volume
+if ( isset( $_POST[ 'volume' ] ) ) {
+	// normal
+	if ( $_POST[ 'volume' ] != -1 ) {
+		sendMpdCommand( $mpd, 'setvol '.$_POST[ 'volume' ] );
+		$redis->set( 'volumemute', 0 );
+		die();
+	}
+	// mute / unmute
+	$volumemute = $redis->get( 'volumemute' );
+	if ( $volumemute == 0 ) {
+		$cmdlist = "command_list_begin\n"
+			."status\n"
+			."setvol 0\n"
+			."command_list_end";
+		sendMpdCommand( $mpd, $cmdlist );
+		$status = readMpdResponse( $mpd );
+		$vol = arrayLines( $status )[ 'volume' ];
+		$redis->set( 'volumemute', $vol );
+		echo $vol;
+	} else {
+		sendMpdCommand( $mpd, 'setvol '.$volumemute );
+		$redis->set( 'volumemute', 0 );
+		echo $volumemute;
+	}
+	die();
+}
