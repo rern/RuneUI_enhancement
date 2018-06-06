4f8eb4461c75607cd6240d8d6d68e3a5b3c69d91
rern <rernrern@gmail.com>
2018-06-06 22:06:38 +0700

    Add files via upload

--- srv/http/assets/js/enhance.js
+++ srv/http/assets/js/enhance.js
@@ -875,47 +875,46 @@ function renderLibraryHome() {
 	$( '#home-blocks, #db-homeSetup' ).removeClass( 'hide' );
 	$( '#db-homeSetup' ).removeClass( 'btn-primary' ).addClass( 'btn-default' );
 	var obj = GUI.libraryhome,
-	i = 0,
-	divOpen = '<div class="col-lg-3 col-md-4 col-sm-6">',
 	toggleSpotify = '',
 	notMPD = ( obj.ActivePlayer === 'Spotify' || obj.ActivePlayer === 'Airplay' );
 	toggleMPD = notMPD ? ' inactive' : '';
-	content = isLocalHost ? '' : '<div class="col-sm-12"><h1 class="txtmid">Browse your library</h1></div>';
 	// Set active player
 	setPlaybackSource();
-
+	
+	var content = '<br>';
+	var divOpen = '<div class="col-lg-3 col-md-4 col-sm-6">';
 	for ( i = 0; ( bookmark = obj.bookmarks[ i ] ); i++ ) {
-		content += divOpen +'<div id="home-bookmark-'+ bookmark.id +'" class="home-block home-bookmark'+ toggleMPD +'" data-path="'+ bookmark.path +'"><i class="fa fa-star"></i><h3>' + bookmark.name + '</h3>bookmark</div></div>';
+		content += divOpen +'<div id="home-bookmark-'+ bookmark.id +'" class="home-block home-bookmark'+ toggleMPD +'" data-path="'+ bookmark.path +'"><i class="fa fa-star"></i><h4>' + bookmark.name + '</h4></div></div>';
 	}
 	if ( chkKey( obj.networkMounts ) ) {
 		content += divOpen +'<a id="home-nas" class="home-block'+ toggleMPD +'"'+ ( obj.networkMounts === 0 ? ( notMPD ? '' : ' href="/sources/add/"' ) : ' data-path="NAS"' ) +'>';
-		content += '<i class="fa fa-sitemap"></i><h3>Network mounts (' + obj.networkMounts + ')</h3>'+ ( isLocalHost ? '' : 'network attached storages</a></div>' );
+		content += '<i class="fa fa-sitemap"></i><h4>Network mounts <span>(' + obj.networkMounts + ')</span></h4></a></div>';
 	}
 	if ( chkKey( obj.localStorages ) ) {
-		content += ( obj.localStorages === 0 ) ? '' : divOpen +'<div id="home-local" class="home-block'+ toggleMPD +'" data-path="LocalStorage"><i class="fa fa-hdd-o"></i><h3>LocalStorage ('+ obj.localStorages +')</h3>'+ ( isLocalHost ? '' : 'locally stored music</div></div>' );
+		content += ( obj.localStorages === 0 ) ? '' : divOpen +'<div id="home-local" class="home-block'+ toggleMPD +'" data-path="LocalStorage"><i class="fa fa-hdd-o"></i><h4>LocalStorage <span>('+ obj.localStorages +')</span></h4></div></div>';
 	}
 	if ( chkKey( obj.USBMounts ) ) {
 		content += divOpen +'<div id="home-usb" class="home-block'+ toggleMPD +'"'+ ( obj.USBMounts === 0 ? ( notMPD ? '' : ' href="/sources/sources/"' ) : ' data-path="USB"' ) +'>';
-		content += '<i class="fa fa-hdd-o"></i><h3>USB storage ('+ obj.USBMounts +')</h3>'+ ( isLocalHost ? '' : 'USB storage</div></div>' );
+		content += '<i class="fa fa-hdd-o"></i><h4>USB drives <span>('+ obj.USBMounts +')</span></h4></div></div>';
 	}
 	if ( chkKey( obj.webradio ) ) {
-		content += divOpen +'<div id="home-webradio" class="home-block'+ toggleMPD +'" data-path="Webradio"><i class="fa fa-microphone"></i><h3>Webradios ('+ obj.webradio +')</h3>'+ ( isLocalHost ? '' : 'webradio local playlists</div></div>' );
+		content += divOpen +'<div id="home-webradio" class="home-block'+ toggleMPD +'" data-path="Webradio"><i class="fa fa-microphone"></i><h4>Webradios <span>('+ obj.webradio +')</span></h4></div></div>';
 	}
-	content += divOpen +'<div id="home-albums" class="home-block'+ toggleMPD +'" data-path="Albums" data-browsemode="album"><i class="fa fa-stop"></i><h3>Albums</h3>'+ ( isLocalHost ? '' : 'browse MPD database by album</div></div>' );
-	content += divOpen +'<div id="home-artists" class="home-block'+ toggleMPD +'" data-path="Artists" data-browsemode="artist"><i class="fa fa-user"></i><h3>Artists</h3>'+ ( isLocalHost ? '' : 'browse MPD database by artist</div></div>' );
-	content += divOpen +'<div id="home-composer" class="home-block'+ toggleMPD +'" data-path="Composer" data-browsemode="composer"><i class="fa fa-pencil"></i><h3>Composer</h3>'+ ( isLocalHost ? '' : 'browse MPD database by composer</div></div>' );
-	content += divOpen +'<div id="home-genre" class="home-block' + toggleMPD +'" data-path="Genres" data-browsemode="genre"><i class="fa fa-tag"></i><h3>Genres</h3>'+ ( isLocalHost ? '' : 'browse MPD database by genre</div></div>' );
+	content += divOpen +'<div id="home-albums" class="home-block'+ toggleMPD +'" data-path="Albums" data-browsemode="album"><i class="fa fa-stop"></i><h4>Albums</h4></div></div>';
+	content += divOpen +'<div id="home-artists" class="home-block'+ toggleMPD +'" data-path="Artists" data-browsemode="artist"><i class="fa fa-user"></i><h4>Artists</h4></div></div>';
+	content += divOpen +'<div id="home-composer" class="home-block'+ toggleMPD +'" data-path="Composer" data-browsemode="composer"><i class="fa fa-pencil"></i><h4>Composers</h4></div></div>';
+	content += divOpen +'<div id="home-genre" class="home-block' + toggleMPD +'" data-path="Genres" data-browsemode="genre"><i class="fa fa-tag"></i><h4>Genres</h4></div></div>';
 	if ( chkKey( obj.Spotify ) && obj.Spotify !== '0' ) {
 		if (obj.ActivePlayer !== 'Spotify') {
-			content += divOpen +'<div id="home-spotify-switch" class="home-block"><i class="fa fa-spotify"></i><h3>Spotify</h3>'+ ( isLocalHost ? '' : 'click to switch renderer</div></div>' );
+			content += divOpen +'<div id="home-spotify-switch" class="home-block"><i class="fa fa-spotify"></i><h4>Spotify</h4></div></div>';
 		} else {
-			content += divOpen +'<div id="home-spotify" class="home-block'+ toggleSpotify +'" data-plugin="Spotify" data-path="Spotify"><i class="fa fa-spotify"></i><h3>Spotify</h3>'+ ( isLocalHost ? '' : 'music for everyone</div></div>' );
+			content += divOpen +'<div id="home-spotify" class="home-block'+ toggleSpotify +'" data-plugin="Spotify" data-path="Spotify"><i class="fa fa-spotify"></i><h4>Spotify</h4></div></div>';
 		}
 	}
 	if ( chkKey( obj.Dirble ) ) {
-		content += divOpen +'<div id="home-dirble" class="home-block'+ toggleMPD +'" data-plugin="Dirble" data-path="Dirble"><i class="fa fa-globe"></i><h3>Dirble</h3>'+ ( isLocalHost ? '' : 'radio stations open directory</div></div>' );
+		content += divOpen +'<div id="home-dirble" class="home-block'+ toggleMPD +'" data-plugin="Dirble" data-path="Dirble"><i class="fa fa-globe"></i><h4>Dirble</h4></div></div>';
 	}
-	content += divOpen +'<div id="home-jamendo" class="home-block'+ toggleMPD +'" data-plugin="Jamendo" data-path="Jamendo"><i class="fa fa-play-circle-o"></i><h3>Jamendo<span id="home-count-jamendo"></span></h3>'+ ( isLocalHost ? '' : 'world\'s largest platform for free music</div></div>' );
+	content += divOpen +'<div id="home-jamendo" class="home-block'+ toggleMPD +'" data-plugin="Jamendo" data-path="Jamendo"><i class="fa fa-play-circle-o"></i><h4>Jamendo<span id="home-count-jamendo"></span></h4></div></div>';
 
 	content += '</div>';
 	document.getElementById( 'home-blocks' ).innerHTML = content;
