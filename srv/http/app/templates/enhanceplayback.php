8f7f3c2ece0b4a066833121a146e8529c62dd2ec
rern <rernrern@gmail.com>
2018-06-06 22:05:51 +0700

    Add files via upload

--- srv/http/app/templates/enhanceplayback.php
+++ srv/http/app/templates/enhanceplayback.php
@@ -119,7 +119,7 @@ echo $li.str_repeat( "<li>&nbsp;</li>\n", 5 );
 		<div class="btnlist btnlist-top">
 			<button id="pl-filter-results" class="btn hide" type="button" title="Close filter results and go back to the playing Queue"><i class="fa fa-times sx"></i></button>
 			<div id="pl-manage">
-				<i id="pl-manage-list" class="fa fa-file-text-o fa-lg" title="Manage playlists"></i>
+				<i id="pl-manage-list" class="fa fa-list-ol fa-lg" title="Manage playlists"></i>
 				<i id="pl-manage-save" class="fa fa-save fa-lg" title="Save current queue as playlist" data-toggle="modal" data-target="#modal-pl-save"></i>
 				<!--<i id="pl-import-youtube" class="fa fa-youtube-play" title="Import a playlist or video from youtube." data-toggle="modal" data-target="#modal-pl-youtube"></i>-->
 				<i id="pl-manage-clear" class="fa fa-trash-o fa-lg" title="Clear the playing queue" data-toggle="modal" data-target="#modal-pl-clear"></i>
