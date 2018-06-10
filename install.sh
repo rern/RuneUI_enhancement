6e93e8f06f13ae8a5754d0a419a6fefa4a8e70c2
rern <rernrern@gmail.com>
2018-06-10 20:11:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -212,7 +212,7 @@ fi
 # set library home database
 if [[ $( redis-cli keys display ) == '' ]]; then
 	redis-cli hmset display bar checked pause checked time checked coverart checked volume checked buttons checked source checked\
-	\nas checked usb checked webradio checked albums checked artists checked composer checked genre checked spotify checked dirble checked jamendo checked &> /dev/null
+	\nas checked sd checked usb checked webradio checked albums checked artists checked composer checked genre checked spotify checked dirble checked jamendo checked &> /dev/null
 fi
 
 installfinish $@
