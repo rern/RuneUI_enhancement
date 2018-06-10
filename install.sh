8d26dee1eaafdb1f6afda6f2f59e2d49382f9613
rern <rernrern@gmail.com>
2018-06-10 10:37:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,7 @@ alias=enha
 installstart $@
 
 # temp fix: remove uninstall leftover
-sed -i -e 's|fa-music sx"></i> Library\(.\);|fa-folder-open"></i>\1|
-' -e 's|</i>"></i>|</i>|
+sed -i 's|fa-music sx"></i> Library\(.\);|fa-folder-open"></i>\1|
 ' /srv/http/assets/js/runeui.js
 
 mv /srv/http/app/coverart_ctl.php{,.backup}
