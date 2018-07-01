000dc83cfddde469a042eb8191a9680918815126
rern <rernrern@gmail.com>
2018-07-01 09:59:04 +0700

    Add files via upload

--- srv/http/app/templates/enhanceplayback.php
+++ srv/http/app/templates/enhanceplayback.php
@@ -128,7 +128,7 @@ echo $li.str_repeat( "<li>&nbsp;</li>\n", 5 );
 				<i id="pl-manage-list" class="fa fa-list-ol fa-lg" title="Manage playlists"></i>
 				<i id="pl-manage-save" class="fa fa-save fa-lg" title="Save current queue as playlist" data-toggle="modal" data-target="#modal-pl-save"></i>
 				<!--<i id="pl-import-youtube" class="fa fa-youtube-play" title="Import a playlist or video from youtube." data-toggle="modal" data-target="#modal-pl-youtube"></i>-->
-				<i id="pl-manage-clear" class="fa fa-trash-o fa-lg" title="Clear the playing queue" data-toggle="modal" data-target="#modal-pl-clear"></i>
+				<i id="pl-manage-clear" class="fa fa-minus-circle fa-lg" title="Clear the playing queue" data-toggle="modal" data-target="#modal-pl-clear"></i>
 			</div>
 			<span id="pl-count" class="hide"></span>
 			<form id="pl-search" class="form-inline" method="post" onSubmit="return false;" role="form">
