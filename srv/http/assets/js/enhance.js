f965d576c8ae50363b9a8ba82cba5a3ea525bdd5
rern <rernrern@gmail.com>
2018-07-21 16:56:41 +0700

    Update enhance.js

--- srv/http/assets/js/enhance.js
+++ srv/http/assets/js/enhance.js
@@ -30,26 +30,6 @@ if ( /\/.*\//.test( location.pathname ) === true ) {
 	return;
 }
 
-var pushstreamAirplay = new PushStream( {
-	host: window.location.hostname,
-	port: window.location.port,
-	modes: GUI.mode
-} );
-pushstreamAirplay.addChannel( 'airplay' );
-pushstreamAirplay.onmessage = function( status ) { // on receive broadcast
-	var status = status[ 0 ];
-	var artist = atob( status[ '61736172' ] ); // arar = string from hex 61736172
-	var title = atob( status[ '6d696e6d' ] );  // minm
-	var album = atob( status[ '6173616c' ] );  // asal
-	var time = atob( status[ '70726772' ] );   // prgr
-	var coverart = status[ '50494354' ];       // PICT - base64 string
-	var time = time.split( '/' );
-	var total = converthms( ( time[ 2 ] - time[ 1 ] ) / 44100 );
-	var elapsed = converthms( ( time[ 0 ] - time[ 1 ] ) / 44100 );
-//	console.log( elapsed +' ; '+ total )
-};
-pushstreamAirplay.connect();
-
 function menubottom( elshow, elhide1, elhide2 ) {
 	$( '#menu-top, #menu-bottom' ).addClass( 'hide' );
 	if ( $( '#panel-sx' ).hasClass( 'active' ) ) librarytop = $( window ).scrollTop();
