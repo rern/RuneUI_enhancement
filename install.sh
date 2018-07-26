fe68637dc18ed6f107981e298267627b7f7dfb70
rern <rernrern@gmail.com>
2018-07-26 13:11:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -317,7 +317,7 @@ file=/srv/http/app/templates/enhanceplayback.php  # for rune youtube
 
 # set library home database
 if [[ $1 != u ]]; then
-	redis-cli hmset display bar checked pause checked time checked coverart checked volume checked buttons checked \
+	redis-cli hmset display bars checked pause checked time checked coverart checked volume checked buttons checked \
 	\nas checked sd checked usb checked webradio checked albums checked artists checked composer checked genre checked \
 	\spotify checked dirble checked jamendo checked &> /dev/null
 fi
