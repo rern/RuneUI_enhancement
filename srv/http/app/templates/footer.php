40f0cad5bbacc2e1ef2a90d956e8df5b349c4fc7
rern <rernrern@gmail.com>
2018-08-04 08:50:22 +0700

    Update footer.php

--- srv/http/app/templates/footer.php
+++ srv/http/app/templates/footer.php
@@ -2,12 +2,12 @@
 	<div id="loaderbg"></div>
 	<div id="loadercontent"><img src="<?=$this->asset('/img/runelogo.svg')?>"></div>
 </div>
+<script src="<?=$this->asset('/js/vendor/jquery-2.1.0.min.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/jquery.mobile.custom.min.js')?>"></script>
 <?php if (is_localhost()): ?>
 <script src="<?=$this->asset('/js/vendor/jquery.onScreenKeyboard.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/jquery-ui.min.js')?>"></script>
 <?php endif ?>
-<script src="<?=$this->asset('/js/vendor/jquery-2.1.0.min.js')?>"></script>
-<script src="<?=$this->asset('/js/vendor/jquery.mobile.custom.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/pushstream.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/bootstrap.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/bootstrap-select.min.js')?>"></script>
