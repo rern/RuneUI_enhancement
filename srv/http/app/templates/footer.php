da76b1e01734404ff01c9a7cc7af617fcf7d90d8
rern <rernrern@gmail.com>
2018-08-07 13:57:25 +0700

    Add files via upload

--- srv/http/app/templates/footer.php
+++ srv/http/app/templates/footer.php
@@ -4,29 +4,33 @@
 </div>
 <script src="<?=$this->asset('/js/vendor/jquery-2.1.0.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/jquery.mobile.custom.min.js')?>"></script>
-<?php if (is_localhost()): ?>
-<script src="<?=$this->asset('/js/vendor/jquery.onScreenKeyboard.js')?>"></script>
-<script src="<?=$this->asset('/js/vendor/jquery-ui.min.js')?>"></script>
-<?php endif ?>
 <script src="<?=$this->asset('/js/vendor/pushstream.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/bootstrap.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/pnotify.custom.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/openwebapp.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/modernizr-custom.js')?>"></script>
-<?php if ( $_SERVER['REQUEST_URI'] !== '/gpiosettings' ): ?>
+
+<?php if (is_localhost()): ?>
+<script src="<?=$this->asset('/js/vendor/jquery.onScreenKeyboard.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/jquery-ui.min.js')?>"></script>
+<?php endif ?>
+<?=( $this->section == 'debug' ? '<script src="'.$this->asset('/js/vendor/ZeroClipboard.min.js').'"></script>' : '' ) ?>
+
+<?php if ( empty( $this->uri(1) ) ): ?>
 <script src="<?=$this->asset('/js/runeui'.( $this->dev === '0' ? '.min' : '' ).'.js')?>"></script>
-<?php endif ?>
-<script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
 <script src="<?=$this->asset('/js/addonsmenu.js')?>"></script>
-<?php if ( $_SERVER['REQUEST_URI'] === '/addons' ): ?>
-<script src="<?=$this->asset('/js/addons.js')?>"></script>
-<?php endif ?>
-<?php if ( $_SERVER['REQUEST_URI'] !== '/gpiosettings' ): ?>
-<script src="<?=$this->asset('/js/vendor/bootstrap-select.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/roundslider.min.js')?>"></script>
 <script src="<?=$this->asset('/js/enhance.js')?>"></script>
-<?php /*gpio<script src="<?=$this->asset('/js/gpio.js')?>"></script>gpio*/ ?>
+<?php /*gpio<script src="<?=$this->asset('/js/enhance.js')?>"></script>gpio*/ ?>
 <?php else: ?>
+<script src="<?=$this->asset('/js/vendor/bootstrap-select.min.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/parsley.min.js')?>"></script>
+<?php endif ?>
+
+<script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
+
+<?=( $this->uri(1) === 'addons' ? '<script src="<'.$this->asset('/js/addons.js').'"></script>' : '' ) ?>
+<?php if ( $this->uri(1) === 'gpiosettings' ): ?>
 <script src="assets/js/vendor/bootstrap-select-1.12.1.min.js"></script>
 <script src="assets/js/gpiosettings.js"></script>
 <?php endif ?>
