d867b02742646de0d30d7ec780b047e0f6fe1244
rern <rernrern@gmail.com>
2018-08-07 18:52:43 +0700

    Update footer.php

--- srv/http/app/templates/footer.php
+++ srv/http/app/templates/footer.php
@@ -26,6 +26,10 @@
 <script src="<?=$this->asset('/js/vendor/jquery.mobile.custom.min.js')?>"></script>
 <script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
 <script src="<?=$this->asset('/js/addons.js')?>"></script>
+<script>
+// restore after update changed to spinning gear
+	$( '#loader' ).html( '<img src="'<?=$this->asset('/img/runelogo.svg')?>'">' );
+</script>
 
 <?php elseif ( $this->uri(1) === 'gpiosettings' ): ?>
 <script src="<?=$this->asset('/js/vendor/bootstrap.min.js')?>"></script>
