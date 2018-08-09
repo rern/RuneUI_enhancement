e4d0dc2b1e6a13b993bb5c465108c2601542fb8d
rern <rernrern@gmail.com>
2018-08-09 13:09:52 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -29,11 +29,11 @@ rm -v /srv/http/assets/css/{enhance.css,midori.css,roundslider.min.css}
 rm -v /srv/http/assets/fonts/enhance*
 rm -v /srv/http/assets/img/{controls*,runelogo.svg,vu*}
 rm -v /srv/http/assets/js/enhance.js
+rm -v /srv/http/assets/js/runeui.min.js
 rm -v /srv/http/assets/js/vendor/{jquery-ui.min.js,modernizr-custom.js,roundslider.min.js}
 rm /usr/share/bootsplash/{start,reboot,shutdown}-runeaudio.png
 
 mv /srv/http/app/coverart_ctl.php{.backup,}
-mv /srv/http/assets/js/runeui.js{.backup,}
 mv /srv/http/assets/js/runeui.min.js{.backup,}
 mv /srv/http/command/airplay_toggle{.backup,}
 mv /usr/share/bootsplash/start-runeaudio.png{.backup,}
