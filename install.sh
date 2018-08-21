5ccd7829a70c5d5fbe4ec836cb2a58af6613b26f
rern <rernrern@gmail.com>
2018-08-21 18:20:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -185,15 +185,15 @@ installfinish $@
 
 # set library home database
 if [[ $1 != u ]]; then
-	[[ $( redis-cli get volume ) == 1 ]] && volume=1 || volume=''
-	
 	redis-cli hmset display bars checked pause checked time checked coverart checked volume checked buttons checked \
 	\nas checked sd checked usb checked webradio checked albums checked artists checked composer checked genre checked \
-	\spotify checked dirble checked jamendo checked volumempd $volume &> /dev/null
-	
-	reinitsystem
-else
-	clearcache
+	\spotify checked dirble checked jamendo checked &> /dev/null
+
+#	[[ $( redis-cli get volume ) == 1 ]] && volume=1 || volume=''
+#	redis-cli hset display volumempd $volume
+#	reinitsystem
+#else
+#	clearcache
 fi
 
 # temp for version before 20180822
