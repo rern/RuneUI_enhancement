6a244b038f88ceb859c99067a7bda2ed3b0215a0
rern <rernrern@gmail.com>
2018-08-27 20:01:47 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -28,7 +28,7 @@ rm -v /srv/http/app/templates/{footer,header,playback}.php
 rm -v /srv/http/assets/css/{bootstrap.min,enhance,fontawesome.min,midori,pnotify.custom.min,roundslider.min,settings,toggle-switch.min}.css
 rm -v /srv/http/assets/fonts/enhance*
 rm -v /srv/http/assets/img/{controls*,runelogo.svg,vu*}
-rm -v /srv/http/assets/js/{enhance,runeuisettings}.js
+rm -v /srv/http/assets/js/{enhance,settings}.js
 rm -v /srv/http/assets/js/vendor/{jquery-ui.min,modernizr-custom,pnotify.custom.min,roundslider.min}.js
 rm /usr/share/bootsplash/{start,reboot,shutdown}-runeaudio.png
 
