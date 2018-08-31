01e37963d71be9df0fd1ba8fdcf68040a4493b91
rern <rernrern@gmail.com>
2018-08-31 09:26:45 +0700

    Add files via upload

--- srv/http/enhance.php
+++ srv/http/enhance.php
@@ -3,9 +3,11 @@
 // volume : N ... mute/unmute: N = -1 )
 // mpd    : mpd protocol command
 
-if ( isset( $_POST[ 'redis' ] ) ) {
+if ( !isset( $_POST[ 'mpd' ] ) ) {
 	$redis = new Redis(); 
 	$redis->pconnect( '127.0.0.1' );
+}
+if ( isset( $_POST[ 'redis' ] ) ) {
 	$array = json_decode( $_POST[ 'redis' ], true );
 	foreach ( $array as $field => $arg ) {
 		$count = count( $arg );
@@ -21,21 +23,12 @@ if ( isset( $_POST[ 'redis' ] ) ) {
 			$result[ $field ] = $redis->$command( $arg[ 1 ], $arg[ 2 ], $arg[ 3 ] );
 		}
 	}
-	$stats = shell_exec( "mpc stats | head -n 3 | tr -d ' '" );
-	$stats = explode( "\n", $stats );
-	foreach( $stats as $stat ) {
-		$kv = explode( ':', $stat );
-		// only with value
-		if ( $kv[ 1 ] ) $counts[ $kv[ 0 ] ] = number_format( $kv[ 1 ] );
-	}
-	$result[ 'counts' ] = $counts;
 	echo json_encode( $result );
 	
 	// broadcast to all clients on hmSet display or set volume
 	if ( !isset( $pushstream ) ) die();
 	
 	$result[ 'display' ] = $redis->hGetAll( 'display' );
-	
 	if ( isset( $airplay ) ) $result[ 'actplayerinfo' ] = $redis->get( 'act_player_info' );
 	$ch = curl_init( 'http://localhost/pub?id=display' );
 	curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json' ) );
@@ -43,8 +36,6 @@ if ( isset( $_POST[ 'redis' ] ) ) {
 	curl_exec( $ch );
 	curl_close( $ch );
 } else if ( isset( $_POST[ 'volume' ] ) ) {
-	$redis = new Redis(); 
-	$redis->pconnect( '127.0.0.1' );
 	// normal
 	$volume = $_POST[ 'volume' ];
 	if ( $volume != -1 ) {
@@ -77,4 +68,11 @@ if ( isset( $_POST[ 'redis' ] ) ) {
 } else if ( isset( $_POST[ 'bash' ] ) ) {
 	$result = shell_exec( '/usr/bin/sudo '.$_POST[ 'bash' ] );
 	echo $result;
+} else if ( isset( $_POST[ 'power' ] ) ) {
+	$cmd = '';
+	if ( file_exists( '/root/gpiooff.py' ) ) $cmd.= '/root/gpiooff.py;';
+	if ( $redis->get( local_browser ) === '1' ) $cmd .= 'killall Xorg; ply-image /srv/http/assets/img/bootsplash.png;';
+	$cmd.= 'umount -f -a -t cifs nfs -l;';
+	$reboot = $_POST[ 'power' ] === 'reboot' ? '-r' : '-h';
+	exec( '/usr/bin/sudo '.$cmd.' shutdown '.$reboot.' now' );
 }
