1d3049c2c8f80ed109aae6003271db78e0e1cafb
rern <rernrern@gmail.com>
2018-09-02 13:17:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,17 +64,6 @@ EOF
 )
 append 'echo getPlayQueue($mpd)'
 #----------------------------------------------------------------------------------
-file=/srv/http/command/rune_PL_wrk
-echo $file
-
-comment -n +2 'JSON response for GUI'
-
-string=$( cat <<'EOF'
-                    ui_render('playback', 1);
-EOF
-)
-append -n +2 'JSON response for GUI'
-#----------------------------------------------------------------------------------
 file=/srv/http/app/libs/runeaudio.php
 echo $file
 
