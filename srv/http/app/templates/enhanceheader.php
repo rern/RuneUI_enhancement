4a9881fbd52faeeae2972eaf61651525807b9cf0
rern <rernrern@gmail.com>
2018-09-04 09:24:40 +0700

    Add files via upload

--- srv/http/app/templates/enhanceheader.php
+++ srv/http/app/templates/enhanceheader.php
@@ -107,13 +107,13 @@ if ( empty( $this->uri(1) ) ) { ?>
 <div id="menu-top" class="hide">
 <i id="menu-settings" class="fa fa-gear"></i>
 <div class="playback-controls">
-	<button id="previous" class="btn btn-default btn-cmd" title="Previous" data-cmd="previous"><i class="fa fa-step-backward"></i></button>
-	<button id="stop" class="btn btn-default btn-cmd" title="Stop" data-cmd="stop"><i class="fa fa-stop"></i></button>
-	<button id="play" class="btn btn-default btn-cmd" title="Play/Pause" data-cmd="play"><i class="fa fa-play"></i></button>
-	<button id="pause" class="btn btn-default btn-cmd" title="Pause" data-cmd="pause"><i class="fa fa-pause"></i></button>
-	<button id="next" class="btn btn-default btn-cmd" title="Next" data-cmd="next"><i class="fa fa-step-forward"></i></button>
+	<button id="previous" class="btn btn-default btn-cmd"><i class="fa fa-step-backward"></i></button>
+	<button id="stop" class="btn btn-default btn-cmd"><i class="fa fa-stop"></i></button>
+	<button id="play" class="btn btn-default btn-cmd"><i class="fa fa-play"></i></button>
+	<button id="pause" class="btn btn-default btn-cmd"><i class="fa fa-pause"></i></button>
+	<button id="next" class="btn btn-default btn-cmd"><i class="fa fa-step-forward"></i></button>
 	</div>
-	<a href="http://www.runeaudio.com/forum/raspberry-pi-f7.html" target="_blank" alt="RuneAudio Forum">
+	<a href="http://www.runeaudio.com/forum/raspberry-pi-f7.html" target="_blank">
 		<img class="logo" src="<?=$this->asset( '/img/runelogo.svg' )?>">
 	</a>
 </div>
