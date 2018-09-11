4594249e2e877d8e393a036f8cc21eda36284da2
rern <rernrern@gmail.com>
2018-09-11 18:39:59 +0700

    Add files via upload

--- srv/http/app/templates/enhancefooter.php
+++ srv/http/app/templates/enhancefooter.php
@@ -11,6 +11,8 @@
 <input id="addonsinfojs" type="hidden" value="<?=$this->asset( '/js/addonsinfo.js' )?>">
 <?php }
 if ( file_exists( '/srv/http/assets/js/gpio.js' ) ) { ?>
+<input id="bootstrapmincss" type="hidden" value="<?=$this->asset('/css/bootstrap.min.css')?>">
+<input id="bootstrapselectmincss" type="hidden" value="<?=$this->asset('/css/bootstrap-select.min.css')?>">
 <input id="gpiosettingscss" type="hidden" value="<?=$this->asset( '/css/gpiosettings.css' )?>">
 <input id="gpiosettingsjs" type="hidden" value="<?=$this->asset( '/js/gpiosettings.js' )?>">
 <input id="gpiopin" type="hidden" value="<?=$this->asset( '/img/RPi3_GPIO.svg' )?>">
