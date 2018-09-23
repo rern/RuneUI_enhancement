5d2e4c9bb5c73eab79a845eb648438d44b20621b
rern <rernrern@gmail.com>
2018-09-23 19:04:02 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -45,6 +45,7 @@ echo -e "$bar Restore modified files ..."
 
 files="
 /srv/http/app/settings_ctl.php
+/srv/http/app/libs/runeaudio.php
 /srv/http/app/templates/mpd.php
 /srv/http/app/templates/settings.php
 /srv/http/command/rune_PL_wrk
@@ -60,3 +61,5 @@ chown -R mpd:audio /mnt/MPD/Webradio
 uninstallfinish $@
 
 restartlocalbrowser
+
+reinitsystem
