0cb0abf8efa99ce0b3be4c38f6c5e30534fc6adf
rern <rernrern@gmail.com>
2018-10-01 08:00:55 +0700

    Update enhancefooter.php

--- srv/http/app/templates/enhancefooter.php
+++ srv/http/app/templates/enhancefooter.php
@@ -2,28 +2,31 @@
 <div id="loader" class="hide"><img src="<?=$this->asset( '/img/runelogo.svg' )?>"></div>
 
 <!-- pass cache busting assets to addonsbash.php and gpiosettings.php -->
-<?php if ( $this->uri(1) === 'addons' || file_exists( '/srv/http/gpiosettings.php' ) ) { ?>
+<?php 
+if ( $this->uri(1) === 'addons' || file_exists( '/srv/http/gpiosettings.php' ) ) { ?>
 <input id="favicon" type="hidden" value="<?=$this->asset( '/img/favicon.ico' )?>">
 <input id="addonswoff" type="hidden" value="<?=$this->asset( '/fonts/addons.woff' )?>">
 <input id="addonsttf" type="hidden" value="<?=$this->asset( '/fonts/addons.ttf' )?>">
 <input id="addonsinfocss" type="hidden" value="<?=$this->asset( '/css/addonsinfo.css' )?>">
 <input id="addonscss" type="hidden" value="<?=$this->asset( '/css/addons.css' )?>">
 <input id="addonsinfojs" type="hidden" value="<?=$this->asset( '/js/addonsinfo.js' )?>">
-<?php }
+<?php 
+}
 if ( file_exists( '/srv/http/gpiosettings.php' ) ) { ?>
 <input id="bootstrapmincss" type="hidden" value="<?=$this->asset('/css/bootstrap.min.css')?>">
 <input id="bootstrapselectmincss" type="hidden" value="<?=$this->asset('/css/bootstrap-select.min.css')?>">
 <input id="gpiosettingscss" type="hidden" value="<?=$this->asset( '/css/gpiosettings.css' )?>">
 <input id="gpiosettingsjs" type="hidden" value="<?=$this->asset( '/js/gpiosettings.js' )?>">
 <input id="gpiopin" type="hidden" value="<?=$this->asset( '/img/RPi3_GPIO.svg' )?>">
-<?php } ?>
-
+<?php
+} ?>
 <script src="<?=$this->asset( '/js/vendor/jquery-2.1.0.min.js' )?>"></script>
-
-<?php if ( is_localhost() ) { ?>
+<?php 
+if ( is_localhost() ) { ?>
 <script src="<?=$this->asset( '/js/vendor/jquery.onScreenKeyboard.js' )?>"></script>
 <script src="<?=$this->asset( '/js/vendor/jquery-ui.min.js' )?>"></script>
-<?php }
+<?php
+}
 if ( empty( $this->uri(1) ) ) { ?>
 <script src="<?=$this->asset( '/js/vendor/jquery.mobile.custom.min.js' )?>"></script>
 <script src="<?=$this->asset( '/js/vendor/pushstream.min.js' )?>"></script>
@@ -37,12 +40,15 @@ if ( empty( $this->uri(1) ) ) { ?>
 <script src="<?=$this->asset( '/js/vendor/roundslider.min.js' )?>"></script>
 <script src="<?=$this->asset( '/js/enhance.js' )?>"></script>
 <script src="<?=$this->asset( '/js/enhancecontext.js' )?>"></script>
-	<?php if ( file_exists( '/srv/http/gpiosettings.php' ) ) { ?> 
+<?php
+	if ( file_exists( '/srv/http/gpiosettings.php' ) ) { ?> 
 <script src="<?=$this->asset( '/js/gpio.js' )?>"></script>
-	<?php }
+<?php 
+	}
 	if ( file_exists( '/srv/http/assets/js/lyrics.js' ) ) { ?> 
 <script src="<?=$this->asset( '/js/lyrics.js' )?>"></script>
-	<?php }
+<?php
+	}
 } else if ( $this->uri(1) === 'addons' ) { ?>
 <script src="<?=$this->asset( '/js/vendor/jquery.mobile.custom.min.js' )?>"></script>
 <script src="<?=$this->asset( '/js/addonsinfo.js' )?>"></script>
@@ -51,10 +57,12 @@ if ( empty( $this->uri(1) ) ) { ?>
 // restore after update changed to spinning gear
 	$( '#loader' ).html( '<img src="<?=$this->asset( '/img/runelogo.svg' )?>">' );
 </script>
-<?php } else {
+<?php
+} else {
 	if ( $this->uri(1) === 'sources' || $this->uri(1) === 'network' ) { ?>
 <script src="<?=$this->asset( '/js/vendor/pushstream.min.js' )?>"></script>
-	<?php } ?>
+<?php 
+	} ?>
 <script src="<?=$this->asset( '/js/vendor/bootstrap.min.js' )?>"></script>
 <script src="<?=$this->asset( '/js/vendor/pnotify.custom.min.js' )?>"></script>
 <script src="<?=$this->asset( '/js/vendor/bootstrap-select.min.js' )?>"></script>
@@ -66,5 +74,5 @@ if ( empty( $this->uri(1) ) ) { ?>
 		.css( 'padding-top', '0' )
 		.find( 'h1' ).before( '<a href="/" style="	float: right; margin-top: 20px;"><i class="fa fa-times fa-2x"></i></a>' );
 </script>
-
-<?php } ?>
+<?php 
+} ?>
