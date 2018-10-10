9ce75fc37391e21bb9fd71a518ec391c959fc71c
rern <rernrern@gmail.com>
2018-10-10 23:06:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ if [[ $1 != u ]]; then
 	\count checked label checked nas checked sd checked usb checked webradio checked albums checked artists checked composer checked genre checked \
 	\spotify checked dirble checked jamendo checked &> /dev/null
 else
-	redis-cli hmset display count checked label checked &> /dev/null
+	redis-cli hmset display debug '' dev '' count checked label checked &> /dev/null
 fi
 # fix webradio permission
 chown -R http:http /mnt/MPD/Webradio
