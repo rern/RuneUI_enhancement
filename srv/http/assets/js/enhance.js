f11e1bd912a47f2120d7561a39ba7965ba9e90c1
rern <rernrern@gmail.com>
2018-10-14 15:07:58 +0700

    Update enhance.js

--- srv/http/assets/js/enhance.js
+++ srv/http/assets/js/enhance.js
@@ -339,7 +339,6 @@ $( '.btn-cmd' ).click( function() {
 	$.post( 'enhance.php', { mpc: 'mpc '+ cmd } );
 } );
 $( '#coverTL' ).click( function() {
-	$( '.controls' ).toggleClass( 'hide', !$( this ).hasClass( 'hide' ) );
 	if ( $( '#album' ).text().slice( 0, 4 ) === 'http' ) return;
 	
 	$.post( 'enhancestatus.php', { statusonly: 1 }, function( status ) {
