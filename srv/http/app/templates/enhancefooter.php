1ce964b47f9d7c8e32ad5752b6ed489ecd82d22a
rern <rernrern@gmail.com>
2018-10-18 10:43:55 +0700

    Add files via upload

--- srv/http/app/templates/enhancefooter.php
+++ srv/http/app/templates/enhancefooter.php
@@ -1,4 +1,4 @@
-<div id="starter"><img src="<?=$this->asset( '/img/runelogo.svg' )?>"></div>
+<div id="splash"><img src="<?=$this->asset( '/img/runelogo.svg' )?>"></div>
 <div id="loader" class="hide"><img src="<?=$this->asset( '/img/runelogo.svg' )?>"></div>
 
 <!-- pass cache busting assets to addonsbash.php and gpiosettings.php -->
