b9495b0f0723a0fdd8af2f8ab0db648be6a3d7d2
rern <rernrern@gmail.com>
2019-01-25 20:13:32 +0700

    Add files via upload

--- srv/http/app/templates/enhancefooter.php
+++ srv/http/app/templates/enhancefooter.php
@@ -21,11 +21,11 @@ if ( empty( $this->uri(1) ) ) { ?>
 <script src="<?=$this->asset( '/js/enhance.js' )?>"></script>
 <script src="<?=$this->asset( '/js/enhancecontext.js' )?>"></script>
 <?php
-	if ( file_exists( '/srv/http/addons.php' ) ) { ?> 
+	if ( file_exists( '/srv/http/assets/js/addonsmenu.js' ) ) { ?> 
 <script src="<?=$this->asset( '/js/addonsmenu.js' )?>"></script>
 <?php
 	}
-	if ( file_exists( '/srv/http/gpiosettings.php' ) ) { ?> 
+	if ( file_exists( '/srv/http/assets/js/gpio.js' ) ) { ?> 
 <script src="<?=$this->asset( '/js/gpio.js' )?>"></script>
 <?php 
 	}
