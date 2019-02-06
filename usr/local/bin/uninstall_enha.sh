c5bed85f1ba6bf9657ae7b633479cd5d02aadca9
rern <rernrern@gmail.com>
2019-02-06 18:00:52 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -14,7 +14,7 @@ rm -v /srv/http/enhance*
 rm -v /srv/http/app/templates/enhance*
 rm -v /srv/http/assets/css/{enhance*,fontawesome.min,midori,pnotify.custom.min,roundslider.min,toggle-switch.min}.css
 rm -v /srv/http/assets/fonts/enhance*
-rm -v /srv/http/assets/img/{bootsplash.png,controls*,cover.svg,runelogo.svg,swipe.svg,vu*}
+rm -v /srv/http/assets/img/{bootsplash.png,controls*,cover.svg,runelogo.svg,vu*}
 rm -v /srv/http/assets/js/enhance*
 rm -v /srv/http/assets/js/vendor/roundslider.min.js
 # DO NOT remove - used by other addons
