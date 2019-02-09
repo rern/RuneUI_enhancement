42a04ed9ba245b6368548f63f7d774ce724fbab4
rern <rernrern@gmail.com>
2019-02-09 15:44:36 +0700

    Add files via upload

--- srv/http/enhancecount.sh
+++ srv/http/enhancecount.sh
@@ -5,9 +5,12 @@ if (( $# > 0 )); then
 	albumartist=$( mpc list albumartist | awk NF | wc -l )
 	composer=$( mpc list composer | awk NF | wc -l )
 	genre=$( mpc list genre | awk NF | wc -l )
-	redis-cli hmset mpddb albumartist $albumartist composer $composer genre $genre
+	count="$albumartist $composer $genre"
+	redis-cli set mpddb "$count"
+	[[ $( redis-cli hget display count ) == 'checked' ]] && curl -s -v -X POST 'http://localhost/pub?id=count' -d "\"$count\""
 	exit
 fi
+
 # spotify activePlayer artist album song composer genre nas usb webradio sd
 count=
 count="$count $( redis-cli hget spotify enable )"
@@ -17,8 +20,6 @@ if [[ $( redis-cli hget display count ) != '' ]]; then
 	count="$count $( df | grep "/mnt/MPD/NAS" | wc -l )"
 	count="$count $( df | grep "/mnt/MPD/USB" | wc -l )"
 	count="$count $( redis-cli hkeys webradios | sed '/(empty list or set)/ d' | awk NF | wc -l )"
-	count="$count $( redis-cli hget mpddb albumartist )"
-	count="$count $( redis-cli hget mpddb composer )"
-	count="$count $( redis-cli hget mpddb genre )"
+	count="$count $( redis-cli get mpddb )"
 fi
 echo $count
