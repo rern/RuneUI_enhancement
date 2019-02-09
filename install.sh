868f7231f9e95fa93a39b0ec8e5838923e65cba1
rern <rernrern@gmail.com>
2019-02-09 12:39:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,9 +96,9 @@ string=$( cat <<'EOF'
 	<legend>Library database</legend>
 	<p>Your <a href="/#panel-sx">music library</a> is composed by two main content types: <strong>local sources</strong> and streaming sources.<br>
 	This section lets you configure your local sources, telling <a href="http://www.musicpd.org/" title="Music Player Daemon" rel="nofollow" target="_blank">MPD</a> to scan the contents of <strong>network mounts</strong> and <strong>USB mounts</strong>.</p>
-	<button class="btn btn-lg btn-primary" id="update"><i class="fa fa-refresh sx"></i>Update</button>&ensp;
+	<button class="btn btn-lg btn-primary" id="update"><i class="fa fa-refresh sx"></i>Update</button>
 	<button class="btn btn-lg btn-primary" id="rescan"><i class="fa fa-refresh sx"></i>Rescan</button>
-	<span id="updating"><i class="fa fa-library bl"></i>&emsp;Library updating...</span>
+	<button class="btn btn-default btn-lg" id="updating"><i class="fa fa-library bl"></i>&emsp;Library updating...</button>
 	<span class="help-block">Update: Only changed data and files &emsp; Rescan: All data and files</span>
 EOF
 )
