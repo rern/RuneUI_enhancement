34560f5c9d51ac9759bf31137386545444f3ba45
rern <rernrern@gmail.com>
2019-02-10 09:30:54 +0700

    Add files via upload

--- srv/http/app/templates/enhancebody.php
+++ srv/http/app/templates/enhancebody.php
@@ -18,8 +18,8 @@ foreach( $blocks as $id => $value ) {
 	$browsemode = in_array( $id, array( 'album', 'artist', 'albumartist', 'composer', 'genre' ) ) ? ' data-browsemode="'.$id.'"' : '';
 	$plugin = in_array( $id, array( 'spotify', 'dirble', 'jamendo' ) ) ? ' data-plugin="'.$value[ 0 ].'"' : '';
 	$blockhtml.= '
-	<div class="col-md-3">
-	<div id="home-'.$id.'" class="home-block"'.$browsemode.$plugin.'><a class="lipath">'.$value[ 0 ].'</a><i class="fa fa-'.$value[ 1 ].'"></i><wh>'.$value[ 2 ].'</wh></div>
+	<div class="divhomeblock">
+		<div id="home-'.$id.'" class="home-block"'.$browsemode.$plugin.'><a class="lipath">'.$value[ 0 ].'</a><i class="fa fa-'.$value[ 1 ].'"></i><wh>'.$value[ 2 ].'</wh></div>
 	</div>
 		';
 }
