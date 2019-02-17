08e47f62b3c80a3d8f3a51558acad80d7b688e1f
rern <rernrern@gmail.com>
2019-02-17 21:55:25 +0700

    Add files via upload

--- srv/http/enhancecoverart.php
+++ srv/http/enhancecoverart.php
@@ -26,13 +26,13 @@ function createThumbnail( $path, $pathcoverarts ) {
 		, 'Cover.png', 'Cover.jpg', 'Folder.png', 'Folder.jpg', 'Front.png', 'Front.jpg'
 	);
 	// each directory
-	foreach( $dirs as $dir ) {
+	foreach( $dirs as $dir ) { // >>> dir
 		$dir = "$path/$dir";
 		if ( !is_dir( $dir ) ) continue;
 		
 		$files = array_slice( scandir( $dir ), 2 ); // remove ., ..
-		// each file
-		foreach( $files as $file ) {
+		// each file - process only 1st audio file
+		foreach( $files as $file ) { // >> files
 			$file = "$dir/$file";
 			if ( !is_file( $file ) ) continue;
 				
@@ -40,21 +40,23 @@ function createThumbnail( $path, $pathcoverarts ) {
 			if ( strpos( $mime, 'audio' ) === 0 ) { // only audio file
 				$audioinfo = new AudioInfo();
 				$id3tag = $audioinfo->Info( $file );
-				$tags = $id3tag[ 'tags' ][ 'vorbiscomment' ];
-				$album = $tags[ 'album' ][ 0 ];
-				$artist = $tags[ 'artist' ][ 0 ];
+				$tags = $id3tag[ 'tags' ][ 'vorbiscomment' ] ?: ( $id3tag[ 'tags' ][ 'vorbiscomment' ] ?: $id3tag[ 'tags' ][ 'id3v1' ] );
+				if ( !$tags ) break; // no tags - end foreach $files
+				
+				$album = str_replace( '/', '', $tags[ 'album' ][ 0 ] );
+				$artist = str_replace( '/', '', $tags[ 'artist' ][ 0 ] );
 				$thumbfile = "$pathcoverarts/$album^^$artist.jpg";
-				if ( file_exists( $thumbfile ) ) break; // skip if already exists - end foreach $files
+				if ( file_exists( $thumbfile ) ) break; // thumnail exists - end foreach $files
 				
 				// create thumbnail from coverart file
-				foreach( $coverfiles as $cover ) {
+				foreach( $coverfiles as $cover ) { // > cover
 					$coverfile = "$dir/$cover";
 					if ( file_exists( $coverfile ) ) {
 						exec( '/usr/bin/sudo /usr/bin/convert "'.$coverfile.'" -thumbnail 200x200 -unsharp 0x.5 "'.$thumbfile.'"' );
 						$created = 1;
-						break; // end foreach $coverfiles
+						break; // found and created - end foreach $coverfiles
 					}
-				}
+				}                                  // > cover
 				if ( isset( $created ) || !isset( $id3tag[ 'comments' ][ 'picture' ][ 0 ][ 'data' ] ) ) break; // end foreach $files
 				
 				// create thumbnail from embedded coverart
@@ -65,7 +67,8 @@ function createThumbnail( $path, $pathcoverarts ) {
 				file_put_contents( $coverfile, $coverart );
 				exec( '/usr/bin/sudo /usr/bin/convert "'.$coverfile.'" -thumbnail 200x200 -unsharp 0x.5 "'.$thumbfile.'"' );
 				unlink( $coverfile );
+				break; // 1st audio file processed - end foreach $files
 			}
-		}
-	}
+		}                            // >> files
+	}                          // >>> dir
 }
