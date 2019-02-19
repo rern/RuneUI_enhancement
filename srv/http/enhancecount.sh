2c98d72213fc8bb670e9c6cf65a3bd8eb770b2f2
rern <rernrern@gmail.com>
2019-02-19 09:07:25 +0700

    Add files via upload

--- srv/http/enhancecount.sh
+++ srv/http/enhancecount.sh
@@ -15,8 +15,8 @@ fi
 count=
 count="$count $( mpc stats | head -n3 | awk '{print $2,$4,$6}' )"
 count="$count $( redis-cli get mpddb )"
-count="$count $( df | grep "/mnt/MPD/NAS" | wc -l )"
-count="$count $( df | grep "/mnt/MPD/USB" | wc -l )"
+count="$count $( df | grep '/mnt/MPD/NAS' | wc -l )"
+count="$count $( df | grep '/mnt/MPD/USB' | wc -l )"
 count="$count $( redis-cli hkeys webradios | sed '/(empty list or set)/ d' | awk NF | wc -l )"
 count="$count $( redis-cli hget spotify enable )"
 count="$count $( redis-cli get activePlayer )"
