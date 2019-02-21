dbd7a9e3062a97000c21f4c626c8422f66af04a1
rern <rernrern@gmail.com>
2019-02-21 11:48:05 +0700

    Add files via upload

--- srv/http/enhance.php
+++ srv/http/enhance.php
@@ -477,11 +477,11 @@ function getLibrary() {
 	}
 	$count = exec( '/srv/http/enhancecount.sh' );
 	$count = explode( ' ', $count );
-	$album = $count[ 11 ] ?: $count[ 1 ];
+	$album = isset( $count[ 11 ] ) ? $count[ 11 ] : $count[ 1 ];
 	$status = array(
 		  'bookmark'     => $bookmarks
 		, 'artist'       => $count[ 0 ]
-		, 'album'        => $count[ 1 ]
+		, 'album'        => $count[ 11 ] ?: $count[ 1 ]
 		, 'song'         => $count[ 2 ]
 		, 'albumartist'  => $count[ 3 ]
 		, 'composer'     => $count[ 4 ]
