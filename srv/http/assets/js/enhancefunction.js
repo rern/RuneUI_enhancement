c80cb5fda9776bf71f46231edf693b6c5a4b9ed5
rern <rernrern@gmail.com>
2019-02-27 16:44:46 +0700

    Add files via upload

--- srv/http/assets/js/enhancefunction.js
+++ srv/http/assets/js/enhancefunction.js
@@ -729,8 +729,9 @@ function renderLibrary() {
 }
 function renderBookmarks() {
 	$.post( 'enhance.php', { getbookmark: 1 }, function( bookmarks ) {
+		console.log(bookmarks)
 		var content = '';
-		if ( bookmarks ) {
+		if ( bookmarks.length ) {
 			bookmarks.sort( function( a, b ) {
 				return stripLeading( a.name ).localeCompare( stripLeading( b.name ), undefined, { numeric: true } );
 			} );
@@ -739,7 +740,7 @@ function renderBookmarks() {
 				var name = bookmark.name.replace( /\\/g, '' );
 				var id = name
 					.replace( / /g, '_' )
-					.replace( /[^A-Za-z0-9_- ]+/g, '-' );
+					.replace( /[^A-Za-z0-9_ -]+/g, '-' );
 				var namehtml = '<div class="divbklabel"><span class="bklabel'+ ( bookmark.coverart ? ' hide' : '' ) +'">'+ name +'</span></div>';
 				content += '<div class="divblock bookmark">'
 						  +'	<div id="home-bk-'+ id +'" class="home-block home-bookmark"><a class="lipath">'+ bookmark.path +'</a>'+ coverarthtml + namehtml +'</div>'
