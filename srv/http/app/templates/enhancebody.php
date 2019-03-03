5d6272f9c1f7e2871b07f15517414e974724dc88
rern <rernrern@gmail.com>
2019-03-03 08:00:04 +0700

    Add files via upload

--- srv/http/app/templates/enhancebody.php
+++ srv/http/app/templates/enhancebody.php
@@ -24,7 +24,7 @@ $count = array(
 // bookmarks
 foreach( $bkmarks as $label => $path ) {
 	$sort = stripLeading( $label );
-	$id = preg_replace( array( '/ /', '/[^A-Za-z0-9_-]+/' ), array( '_', '-' ), $label ); // for order
+	$id = str_replace( ' ', '_', $label ); // for order
 	$thumbfile = '/mnt/MPD/'.$path.'/thumbnail.jpg';
 	if ( file_exists( $thumbfile ) ) {
 		$thumbnail = file_get_contents( $thumbfile );
@@ -67,7 +67,7 @@ foreach( $blocks as $id => $value ) {
 				<a class="lipath">'.$value[ 0 ].'</a>
 				<i class="fa fa-'.$value[ 1 ].'"></i>
 				'.$counthtml.'
-				<wh>'.$value[ 2 ].'</wh>
+				<label class="label">'.$value[ 2 ].'</label>
 			</div>
 		</div>
 	';
@@ -85,7 +85,7 @@ foreach( $bookmarks as $bookmark ) {
 			<div id="home-bk-'.$bookmark[ 1 ].'" class="home-block home-bookmark">
 				<a class="lipath">'.$bookmark[ 3 ].'</a>
 				'.$namehtml.'
-				<div class="divbklabel"><span class="bklabel'.$hidelabel.'">'.$bookmark[ 2 ].'</span></div>
+				<div class="divbklabel"><span class="bklabel label'.$hidelabel.'">'.$bookmark[ 2 ].'</span></div>
 			</div>
 		</div>
 	';
