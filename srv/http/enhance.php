a6149fb476b016355ac88afb15b79ad05e00748e
rern <rernrern@gmail.com>
2019-03-11 17:43:57 +0700

    Add files via upload

--- srv/http/enhance.php
+++ srv/http/enhance.php
@@ -328,16 +328,19 @@ if ( isset( $_POST[ 'mpc' ] ) ) {
 	exec( 'mpc volume '.$vol );
 	pushstream( 'volume', array( $vol, $currentvol ) );
 } else if ( isset( $_POST[ 'power' ] ) ) {
-	$mode = $_POST[ 'power' ];
+$mode = $_POST[ 'power' ];
 	$sudo = '/usr/bin/sudo /usr/bin/';
 	$sudosrv = '/usr/bin/sudo /srv/http/';
 	if ( file_exists( '/srv/http/gpio/gpiooff.py' ) ) $cmd.= $sudosrv.'gpio/gpiooff.py;';
 	if ( $redis->get( local_browser ) === '1' ) $cmd .= $sudo.'killall Xorg; /usr/local/bin/ply-image /srv/http/assets/img/bootsplash.png;';
 	$cmd.= $sudo.'umount -f -a -t cifs nfs -l;';
-	exec( $sudo.'mount | /usr/bin/grep -q mmcblk0p8 && /usr/bin/echo 8 > /sys/module/bcm2709/parameters/reboot_part' );
-	$cmd.= $sudo.'shutdown '.( $mode === 'reboot' ? '-r' : '-h' ).' now';
-	exec( $cmd );
-}
+	if ( $mode !== 'screenoff' ) {
+		exec( $sudo.'mount | /usr/bin/grep -q mmcblk0p8 && /usr/bin/echo 8 > /sys/module/bcm2709/parameters/reboot_part' );
+		$cmd.= $sudo.'shutdown '.( $mode === 'reboot' ? '-r' : '-h' ).' now';
+	} else {
+		$cmd = 'export DISPLAY=:0; xset dpms force off';
+	}
+	exec( $cmd );}
 function stripLeading( $string ) {
 	// strip articles | non utf-8 normal alphanumerics , fix: php strnatcmp ignores spaces + tilde for sort last
 	$names = strtoupper( strVal( $string ) );
