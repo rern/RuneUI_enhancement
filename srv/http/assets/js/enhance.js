abf30829d367995eb510263ead9d10ce9bb18e70
rern <rernrern@gmail.com>
2019-03-20 20:40:48 +0700

    Add files via upload

--- srv/http/assets/js/enhance.js
+++ srv/http/assets/js/enhance.js
@@ -645,11 +645,6 @@ $( '.btn-cmd' ).click( function() {
 			}
 			pos = pos || 1;
 			command = GUI.status.state === 'play' ? 'mpc play '+ pos : [ 'mpc play '+ pos, 'mpc stop' ];
-			if ( GUI.status.ext === 'radio' ) {
-				$( '#cover-art' )
-					.attr( 'src', GUI.status.state === 'play' ? vu : vustop )
-					.css( 'border-radius', '18px' );
-			}
 		} else {
 			command = ( GUI.status.ext === 'radio' && GUI.status.state === 'play' ) ? 'mpc stop' : 'mpc toggle';
 		}
