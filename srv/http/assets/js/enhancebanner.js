bd33da2bc2655d562fb64430b50a160edef9ca80
rern <rernrern@gmail.com>
2019-04-18 10:29:03 +0700

    Add files via upload

--- srv/http/assets/js/enhancebanner.js
+++ srv/http/assets/js/enhancebanner.js
@@ -6,13 +6,20 @@ $( 'body' ).prepend(
 );
 $( '#banner' ).click( bannerHide );
 function bannerHide() {
-	$( '#banner' ).hide();
+	$( '#banner' )
+		.hide()
+		.removeAttr( 'style' );
 	$( '#bannerTitle, #bannerMessage' ).empty();
 }
 function notify( title, message, icon, delay ) {
 	var iconhtml = icon ? '<i class="fa fa-'+ ( icon || 'check' ) +'"></i> ' : '';
 	$( '#bannerTitle' ).html( iconhtml + title );
 	$( '#bannerMessage' ).html( message );
-	$( '#banner' ).show();
+	var $banner = $( '#banner' );
+	$banner.show();
+	// keep gradient width at 80px when width varies
+	var bW = $banner.outerWidth();
+	$banner.css( 'background', 'linear-gradient( to right, rgba( 0,149,216,1 ) 0%,rgba( 0,149,216,1 ) '+ Math.round( ( bW - 80 ) / bW * 100 ) +'%,rgba( 125,185,232,0 ) 100% )' )
+	
 	setTimeout( bannerHide, delay || 3000 );
 }
