0d63547d42f01ef6d9d5b7187eac7d31e76c0bf3
rern <rernrern@gmail.com>
2019-05-11 19:07:28 +0700

    Add files via upload

--- srv/http/app/templates/enhancebody.php
+++ srv/http/app/templates/enhancebody.php
@@ -415,15 +415,11 @@ $menu.= '</div>';
 		<i id="db-back" class="fa fa-arrow-left"></i>
 	</div>
 	<div id="home-blocks" class="row" data-count="<?=$counts[ 'song' ]?>">
-		<div id="divhomeblocks">
-			<?=$blockhtml?>
-		</div>
+		<div id="divhomeblocks"><?=$blockhtml?></div>
 	</div>
 	<div id="db-list">
 		<ul id="db-entries" class="database"></ul>
-		<ul id="db-index" class="index hide">
-			<?=$index?>
-		</ul>
+		<ul id="db-index" class="index hide"><?=$index?></ul>
 		<div id="divcoverarts" class="hide"><?=$coverartshtml ?></div>
 	</div>
 </div>
@@ -457,12 +453,9 @@ $menu.= '</div>';
 	<div id="pl-list">
 		<ul id="pl-entries" class="playlist"></ul>
 		<ul id="pl-editor" class="hide"></ul>
-		<ul id="pl-index" class="index hide">
-			<?=$index?>
-		</ul>
-		<div id="playlist-warning" class="playlist hide">
-			<i class="fa fa-plus-circle"></i>
-		</div>
+		<ul id="pl-index" class="index hide"><?=$index?></ul>
+		<br>
+		<i id="playlist-warning" class="fa fa-plus-circle playlist hide"></i>
 	</div>
 </div>
 
