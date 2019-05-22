bfc4047a588b917b9f251fc8c64dae4ee16ab998
rern <rernrern@gmail.com>
2019-05-22 18:39:13 +0700

    Add files via upload

--- srv/http/assets/js/enhancefunction.js
+++ srv/http/assets/js/enhancefunction.js
@@ -1483,7 +1483,7 @@ function dbContextmenu( $li, $target ) {
 	$( '.replace' ).toggleClass( 'hide', !GUI.status.playlistlength );
 	$( '.remove' ).toggleClass( 'hide', $( '.licover' ).length > 0 );
 	$( '.update' ).toggleClass( 'hide', GUI.status.updating_db !== 0 );
-	$( '.tag' ).toggleClass( 'hide', ( $( '.licover' ).length === 0 || GUI.playlist ) === true );
+	$( '.tag' ).toggleClass( 'hide', $( '.licover' ).length === 0 || GUI.browsemode !== 'file' );
 	var contextnum = $menu.find( 'a:not(.hide)' ).length;
 	$( '.menushadow' ).css( 'height', contextnum * 41 - 1 );
 	$( '#'+ dbpl +'-entries li, #pl-editor li' ).removeClass( 'active' );
