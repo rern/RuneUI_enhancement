a8ef313320676ad159599d947d6e87309f4d86da
rern <rernrern@gmail.com>
2019-05-26 21:50:04 +0700

    Add files via upload

--- srv/http/enhancebio.php
+++ srv/http/enhancebio.php
@@ -1,47 +1,63 @@
 <?php
 $redis = new Redis(); 
 $redis->connect( '127.0.0.1' );
-$apikey = $redis->get( 'lastfm_apikey' );
+$apikey_l = $redis->get( 'lastfm_apikey' );
+$apikey_f = '06f56465de874e4c75a2e9f0cc284fa3';
 $artist = $_GET[ 'artist' ];
-
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, 'http://ws.audioscrobbler.com/2.0/?api_key='.$apikey.'&autocorrect=1&format=json&method=artist.getinfo&artist='.urlencode( $artist ) );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 ); //return the transfer as a string 
-$data = curl_exec( $ch );
-curl_close( $ch );
-
-$data = preg_replace( '/\\\\n/', '<br>', $data );
-$data = preg_replace( '/\\\\"/', '&quot;', $data );
-
+$data = curlGet( 'http://ws.audioscrobbler.com/2.0/'
+	.'?autocorrect=1&format=json&method=artist.getinfo'
+	.'&api_key='.$apikey_l.'&artist='.urlencode( $artist ) );
+$data = preg_replace( 
+	  array( '/\\\\n/', '/\\\\"/' )
+	, array( '<br>',    '&quot;')
+	, $data
+);
 $data = json_decode( $data, True );
+
 $data = $data[ 'artist' ];
-$image = $data[ 'image' ][ 5 ][ '#text' ];
 $genre = ucwords( $data[ 'tags' ][ 'tag' ][ 0 ][ 'name' ] );
-$content = preg_replace( '/ <a.*Read more on Last.fm.*/', '', $data[ 'bio' ][ 'content' ] );
+$content = preg_replace(
+	  array( '/^<br><br>/', '/^<br>/', '/ <a.*Read more on Last.fm.*/' )
+	, array( '',            '',        '' )
+	, $data[ 'bio' ][ 'content' ]
+);
 $similar =  $data[ 'similar' ][ 'artist' ];
+
+
+$data = curlGet( 'https://webservice.fanart.tv/v3/music/'
+	.$data[ 'mbid' ].'&?api_key='.$apikey_f );
+$data = json_decode( $data, True );
+$image = $data[ 'artistthumb' ][ 0 ][ 'url' ];
+
 $similars = '';
 foreach ( $similar as $name ) {
 	$similars.= '
 		<div class="biosimilar">
-			<img src="'.$name[ 'image' ][ 2 ][ '#text' ].'"><br>
 			<p>'.$name[ 'name' ].'</p>
 		</div>
 	';
 }
-
 echo '
 	<legend>'.$artist.'</legend>
 	<form class="form-horizontal">
-		<img id="bioimg" src="'.$image.'">
+		<img id="bioimg" src="'.str_replace( '/fanart/', '/preview/', $image ).'">
 		<p>
 			'.$content.'
 		</p>
 		<div style="clear: both;"></div>
 		<br>
-		<a>Genre: </a>'.$genre.'<span style="float: right;">Source: last.fm</span><br>
+		<a>Genre: </a>'.$genre.'<span style="float: right;">Source: last.fm<br>Images: fanart.tv</span><br>
 		<br>
 		<a>Similar Artists:</a> <span>(click for bio of artists)</span><br>
 		<br>
 		'.$similars.'
 	</form>
 ';
+
+function curlGet( $url ) {
+	$ch = curl_init( $url );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	$response = curl_exec( $ch );
+	curl_close( $ch );
+	return $response;
+}
