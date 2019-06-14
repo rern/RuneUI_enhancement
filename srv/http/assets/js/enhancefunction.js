a2f683399e954a7280447ccfb8d1b773c934a62d
rern <rernrern@gmail.com>
2019-06-14 09:43:39 +0700

    Add files via upload

--- srv/http/assets/js/enhancefunction.js
+++ srv/http/assets/js/enhancefunction.js
@@ -2030,6 +2030,13 @@ function getOrientation( file, callback ) { // return: 1 - undefined
 	};
 	reader.readAsArrayBuffer( file.slice( 0, 64 * 1024 ) );
 }
+function rgb2hex( rgb ) {
+	rgb = rgb.match( /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/ );
+	function hex( x ) {
+		return ( '0'+ parseInt( x ).toString( 16 ) ).slice( -2 );
+	}
+	return '#'+ hex( rgb[ 1 ] ) + hex( rgb[ 2 ] ) + hex( rgb[ 3 ] );
+}
 function setColor( color ) {
 	$.post( 'enhance.php', { 
 		bash: '/usr/bin/sed -i "s|#......\\\(/\\\*c\\\*/\\\)|'+ color +'\\1|g" $( grep -ril "\\\/\\\*c\\\*\\\/" /srv/http/assets/{css,js} );'
