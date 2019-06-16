3b1b4ec6bd45bae2e3551410e865bbbe8ab3e6f1
rern <rernrern@gmail.com>
2019-06-16 21:07:32 +0700

    Update uninstall_enha.sh

--- usr/local/bin/uninstall_enha.sh
+++ usr/local/bin/uninstall_enha.sh
@@ -33,11 +33,11 @@ echo -e "$bar Restore modified files ..."
 
 files="
 /boot/config.txt
-/srv/http/app/templates/network_edit.php
 /srv/http/app/settings_ctl.php
 /srv/http/app/libs/runeaudio.php
 /srv/http/app/templates/dev.php
 /srv/http/app/templates/mpd.php
+/srv/http/app/templates/network_edit.php
 /srv/http/app/templates/settings.php
 /srv/http/app/templates/sources.php
 /srv/http/app/templates/sources_edit.php
