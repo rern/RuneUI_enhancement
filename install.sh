46308d814eb0dd9d68e58a57c95381156dc2000e
rern <rernrern@gmail.com>
2019-06-19 09:55:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -193,8 +193,8 @@ fi
 c=$( redis-cli hget display color )
 if [[ -n $c && $c != 'hsl(200,100%,40%)' ]]; then
 	l=$( echo $c | cut -d'%' -f2 | tr -d ',' )
-	ch=$( echo $c | sed "s/%.*%/%,$(( l + 10 ))%/" )
-	ca=$( echo $c | sed "s/%.*%/%,$(( l - 20 ))%/" )
+	ch=$( echo $c | sed "s/%.*%/%,$(( l + 5 ))%/" )
+	ca=$( echo $c | sed "s/%.*%/%,$(( l - 10 ))%/" )
 	sed -i "s|hsl(*\(/\*c\*/\)|$c\1|g; s|hsl(.*\(/\*ch\*/\)|$ch\1|g; s|hsl(.*\(/\*ca\*/\)|$ca\1|g
 	" $( grep -ril '\/\*c' /srv/http/assets/{css,js} )
 fi
